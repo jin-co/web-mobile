8f5e43251e6248201325d393f388fec8df70a663
jin-co <lilijk@hanmail.net>
2021-09-30 01:30:03 +0900

    chore: update

--- L3_Time/index.ts
+++ L3_Time/index.ts
@@ -1,1 +1,14 @@
-let foo: number = 7;
+let foa: any = 7; //don't use any as it defeats the purpose of using typescript
+let fob: number[] = [7, 2]; // using array
+let foc: number[] = []; // empty array
+let fod: number = 7;
+const FOO: number = 7;
+function isEven(input:number): boolean {
+    if (input % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let foe: boolean = isEven(fod);
