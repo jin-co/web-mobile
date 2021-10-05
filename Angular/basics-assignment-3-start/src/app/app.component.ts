710765e22ea0d94d71bc9d409e67d4d17c90478a
jin-co <lilijk@hanmail.net>
2021-10-05 21:03:02 +0900

    feat: complete assignment 3

--- Angular/basics-assignment-3-start/src/app/app.component.ts
+++ Angular/basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .para {
+      color: #fff;
+      background-color: blue;      
+    }
+  `]
 })
 export class AppComponent {
+  isVisible:boolean = false
+
+  count:number = 0
+  numbers = [0]
+  
+  incrementCount() {
+    this.count++
+  }
+
+  addNumbers() {
+    this.numbers.push(this.count)
+    console.log(this.count)
+  }
 }
