f792de832478000939e5767d82c0fbfeada8858f
jin-co <lilijk@hanmail.net>
2021-10-05 20:32:29 +0900

    chore: update

--- Angular/my-first-app/src/app/directives/directives.component.ts
+++ Angular/my-first-app/src/app/directives/directives.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.css']
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class DirectivesComponent implements OnInit {
   directiveName:string = ''
   isTrue = false
   serverStatus = 'offline'
+  servers = ['test server']
   constructor() {
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
   }
@@ -22,5 +27,9 @@ export class DirectivesComponent implements OnInit {
 
   getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red'
+  }
+
+  createServer() {
+    this.servers.push(this.directiveName)
   }
 }
