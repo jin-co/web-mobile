375049c4426f2dc805307fedf04942d1c7385e49
jin-co <lilijk@hanmail.net>
2021-10-06 19:36:51 +0900

    chore: update

--- recipe-shop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('Test', 'a test', 'https://crystalandcomp.com/wp-content/uploads/2018/02/Irish-Nachos-4.jpg')
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.recipes)
   }
 
 }
