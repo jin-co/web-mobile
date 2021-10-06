77b31004aa2b9e8d271e4130116ea0613cfeb7dd
jin-co <lilijk@hanmail.net>
2021-10-06 18:50:41 +0900

    chore: update

---
+++ recipe-shop/src/app/shopping-list/shoppin-edit/shoppin-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppin-edit',
+  templateUrl: './shoppin-edit.component.html',
+  styleUrls: ['./shoppin-edit.component.css']
+})
+export class ShoppinEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
