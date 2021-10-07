e6dc817b25edf733fd14a46fd72af2c2477f781f
jin-co <lilijk@hanmail.net>
2021-10-07 18:03:53 +0900

    chore: update

---
+++ Angular/assignment-4-databinding-start/src/app/game-control/game-control.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  interval;
+  intervalFired = new EventEmitter<number>()
+  lastNumber = 0
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onStart() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1)
+      this.lastNumber++
+    }, 1000)
+  }
+
+  onStop() {
+
+  }
+}
