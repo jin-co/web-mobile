fddf368b3e8b0c1202c808180ac2e7c9ce03edbc
jin-co <lilijk@hanmail.net>
2021-10-08 20:13:21 +0900

    chore: updat

---
+++ Angular/directives-practice/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor:string
+
+  constructor(private elRef:ElementRef, private renderer:Renderer2) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+
+  @HostListener('mouseenter') mouseover(eventData:Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData:Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', '')
+  }
+
+  @HostListener('click') click(eventData:Event) {
+    this.backgroundColor = 'yellow'
+  }
+}
