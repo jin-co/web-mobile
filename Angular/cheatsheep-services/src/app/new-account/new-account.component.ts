7bee6a79ff7cc0b55dd1564c677605fe91b723de
jin-co <lilijk@hanmail.net>
2021-10-12 23:39:46 +0900

    chore: udpate

--- Angular/cheatsheep-services/src/app/new-account/new-account.component.ts
+++ Angular/cheatsheep-services/src/app/new-account/new-account.component.ts
@@ -1,25 +1,34 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../accounts.service';
 import { Logging } from '../loggin.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [Logging, AccountsService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
+  constructor(private loggingService: Logging,
+              private accountService: AccountsService) {
+                
+  }
+
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
     console.log('A server status changed, new status: ' + accountStatus);
 
     /* even though this works, this is not the way how to use a service
        do not create an instances(Angular provides a better way)
+       const service = new Logging()
+       service.logStatusChange(accountStatus)
     */
-    const service = new Logging()
-    service.logStatusChange(accountStatus)
+    this.loggingService.logStatusChange(accountStatus)
+    this.accountService.addAccount(accountName, accountStatus)
   }
 }
