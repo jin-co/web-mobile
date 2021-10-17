a8f15945cdbd97317a035067fbe55eff6376a518
jin-co <lilijk@hanmail.net>
2021-10-17 17:39:08 +0900

    chore: update

---
+++ Angular/cheatsheet-total/src/app/components/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
+})
+
+// @Component({
+//     selector: 'app-user',
+//     template: `
+//     <h1>hi</h1>
+//     `,
+//     styles: [`
+//         h1 {
+//             color: blue
+//         }
+//     `]
+// })
+
+export class UserComponent {
+
+}
+
+
+
