21ce1f1243b9d03117bf4f8f03fae78505d0a1a3
jin-co <lilijk@hanmail.net>
2021-10-18 19:24:25 +0900

    chore: update

--- Angular/cheatsheet-total/src/app/components/user/user.component.ts
+++ Angular/cheatsheet-total/src/app/components/user/user.component.ts
@@ -23,6 +23,9 @@ export class UserComponent {
     firstName: string = 'John'
     lastName: string = 'Doe'
     age: number = 30
+    numbers: number[]
+    typeMixed: any[]
+    tuple: [string, number, boolean]
     address = {
         street: '50 main',
         city: 'Boston',
@@ -33,7 +36,9 @@ export class UserComponent {
     // constructor runs first the template is rendered
     // good for dependencies
     constructor() {
-
+        this.numbers = [1, 2, 3]
+        this.typeMixed = ['mixed', 0, true]
+        this.tuple = ['hello', 1, true]
     }
 
     showAge() {
@@ -46,6 +51,10 @@ export class UserComponent {
 
     hasBirthday() {
         this.age += 1
+    }
+
+    addNumbers(num1: number, num2:number): number {
+        return num1 + num2
     }
 }
 
