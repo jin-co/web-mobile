8131980a9e22539d3f3e3c6e0f7167e4a40cae8c
jin-co <lilijk@hanmail.net>
2021-10-24 22:46:08 +0900

    chore: update

--- Assignment2/src/app/grocery-list/grocery-list.component.ts
+++ Assignment2/src/app/grocery-list/grocery-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-grocery-list',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroceryListComponent implements OnInit {
   pageTitle: string = "Grocery List"
+  
+  // validations
+  quantityCtrl: FormControl = new FormControl(null, Validators.required)
+  itemCtrl: FormControl = new FormControl(null, [
+    Validators.required,
+    Validators.maxLength(16)
+  ])
+
   constructor() { }
 
   ngOnInit(): void {
