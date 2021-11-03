dc1f561bcf5c195903c769510ed90222b7fc9e1d
jin-co <lilijk@hanmail.net>
2021-11-03 13:42:13 +0900

    chore: update

---
+++ Angular/cheatsheet-total/src/app/components/post-details/post-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/models/Post';
+import { PostService } from 'src/app/services/post.service';
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-post-details',
+  templateUrl: './post-details.component.html',
+  styleUrls: ['./post-details.component.css']
+})
+export class PostDetailsComponent implements OnInit {
+  post!: Post;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.postService.getPost(id).subscribe(post => this.post = post)
+  }
+}
