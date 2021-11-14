a3a3aa56acb932aff3137c312e010bc3dea71234
jin-co <lilijk@hanmail.net>
2021-11-14 16:22:36 +0900

    chore: update

---
+++ client-panel/src/app/components/clients/clients.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/client';
+import { ClientService } from '../../services/client.service';
+
+
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+  clients!: Client[];
+  totalOwed!: number;
+
+  constructor(private clientService: ClientService) { }
+
+  ngOnInit() {
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    });
+  }
+
+  getTotalOwed() {
+    this.totalOwed = this.clients.reduce((total, client) => {
+      return total + client.balance;
+    }, 0);
+  }
+}
+
--- client-panel/src/environments/environment.prod.ts
+++ client-panel/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDNskP5xX2R3wJixFoTBR05HyXr5d07RrY",
+    authDomain: "clientpanel-eddee.firebaseapp.com",
+    projectId: "clientpanel-eddee",
+    storageBucket: "clientpanel-eddee.appspot.com",
+    messagingSenderId: "133891744726",
+    appId: "1:133891744726:web:df67bfc6950a384c0efa77",
+    measurementId: "G-RX2CMY4YXR"
+  }
 };
