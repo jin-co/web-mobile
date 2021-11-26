e2565b3135b4ea172904bcd4f65a9674575897f1
jin-co <lilijk@hanmail.net>
2021-11-26 23:16:55 +0900

    chore: add restaurant component

---
+++ order-first/src/app/components/restaurant/r-main/r-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-r-main',
+  templateUrl: './r-main.component.html',
+  styleUrls: ['./r-main.component.scss']
+})
+export class RMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
