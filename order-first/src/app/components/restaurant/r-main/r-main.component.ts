089714ef90d399627e0fad7d26745d53287e42d5
jin-co <lilijk@hanmail.net>
2021-11-27 01:35:37 +0900

    chore: update

--- order-first/src/app/components/restaurant/r-main/r-main.component.ts
+++ order-first/src/app/components/restaurant/r-main/r-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-r-main',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./r-main.component.scss']
 })
 export class RMainComponent implements OnInit {
+  isLinear = false;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
   }
 
 }
+
+// https://www.concretepage.com/angular-2/angular-2-formgroup-example
