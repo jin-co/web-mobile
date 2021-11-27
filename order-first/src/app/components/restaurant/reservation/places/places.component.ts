2cfeffaaae1b28ed0f7bd6952419f8c8b7bffece
jin-co <lilijk@hanmail.net>
2021-11-28 00:55:26 +0900

    chore: complete UI

--- order-first/src/app/components/restaurant/reservation/places/places.component.ts
+++ order-first/src/app/components/restaurant/reservation/places/places.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./places.component.scss']
 })
 export class PlacesComponent implements OnInit {
+  centered = false;
+  disabled = false;
+  unbounded = false;
 
+  radius!: number;
+  color!: string;
   constructor() { }
 
   ngOnInit(): void {
