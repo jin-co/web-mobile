9dc5615f42784059ffc0be3e57c62c69651bb031
jin-co <lilijk@hanmail.net>
2021-11-29 00:59:35 +0900

    chore: add reservation service

--- order-first/src/app/components/restaurant/owners/owners.component.ts
+++ order-first/src/app/components/restaurant/owners/owners.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Menu } from 'src/app/models/menu';
+import { Reservation } from 'src/app/models/reservation';
 import { DataMenuService } from 'src/app/services/data-menu.service';
+import { DataReservationService } from 'src/app/services/data-reservation.service';
 
 @Component({
   selector: 'app-owners',
   templateUrl: './owners.component.html',
   styleUrls: ['./owners.component.scss']
 })
-export class OwnersComponent implements OnInit {
+export class OwnersComponent implements OnInit {  
   menu!:Menu
-  constructor(private menuService:DataMenuService) { }
+  reservation!:Reservation
+
+  constructor(
+    private menuService:DataMenuService,
+    private reservationService:DataReservationService
+    ) { }
 
   ngOnInit(): void {
   }
@@ -17,12 +24,17 @@ export class OwnersComponent implements OnInit {
   onSubmit(e:Event) {
     e.preventDefault()
     console.log(e)
-    this.menu = {
-      restaurantId: 2,
-      name: "h",
-      subName: "d",
-      imageURL: "e",
-      description: ""
-    }
+    this.reservation = {
+      restaurantName: '',
+      detail: {
+        date:'',
+        time:'',
+        tel:''
+      },
+      menuSelected: []
+    };
+
+    // resets the input fields
+    (e.currentTarget as HTMLFormElement).reset()
   }
 }
---
+++ order-first/src/app/services/data-reservation.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataReservationService {
+  reservations!:Reservation[]
+  constructor() { }
+
+  getReservation() {
+    return this.reservations
+  }
+
+  updateReservation() {
+    
+  }
+}
