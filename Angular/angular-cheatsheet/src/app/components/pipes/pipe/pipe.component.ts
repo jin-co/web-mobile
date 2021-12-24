d124cde4caf6050576da91d6274e904b6d0b72f4
jin-co <lilijk@hanmail.net>
2021-12-24 11:55:01 +0900

    chore: update

--- Angular/angular-cheatsheet/src/app/components/pipes/pipe/pipe.component.ts
+++ Angular/angular-cheatsheet/src/app/components/pipes/pipe/pipe.component.ts
@@ -12,6 +12,7 @@ export class PipeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // async pipe
   appStatus = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('stable');
