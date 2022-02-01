ef3da1cbfbfa3b0c1ac0fa2a20ef3dc2d79b2883
jin-co <lilijk@hanmail.net>
2022-02-01 23:14:37 +0900

    chore: update

--- AndroidApps/P2/app/src/main/java/com/example/p2bmi/MainActivity.java
+++ AndroidApps/P2/app/src/main/java/com/example/p2bmi/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.p2bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // calling xml file
+
+        TextView resultText = findViewById(R.id.txt_result);
+//        resultText.setText("result");
+
+        RadioButton radMale = findViewById(R.id.radio_male);
+        RadioButton radFemale = findViewById(R.id.radio_female);
+        EditText txtAge = findViewById(R.id.txt_age);
+        EditText txtFeet = findViewById(R.id.txt_feet);
+        EditText txtInch = findViewById(R.id.txt_inch);
+        EditText txtWeight = findViewById(R.id.txt_weight);
+        Button calculate = findViewById(R.id.btn_cal);
+
+
+        // alert
+//        Toast.makeText(this, "Go", Toast.LENGTH_LONG).show();
     }
 }
