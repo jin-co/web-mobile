8ac163c2f0804e89de45a4f7b883acc1a7d27423
jin-co <lilijk@hanmail.net>
2022-02-03 22:46:56 +0900

    chore: update

--- AndroidApps/HungryAngry/app/src/main/java/com/example/hungryangry/MainActivity.java
+++ AndroidApps/HungryAngry/app/src/main/java/com/example/hungryangry/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.hungryangry;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Fields
+    CardView starterCard;
+    CardView mainCard;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        starterCard = findViewById(R.id.card_view_starters);
+        mainCard = findViewById(R.id.card_view_main);
     }
 }
+
+/*
+* https://developer.android.com/jetpack/androidx/releases/cardview
+*
+*
+* */
