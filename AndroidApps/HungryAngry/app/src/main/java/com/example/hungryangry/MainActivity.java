f85fcfcec71c5d8fdac4b4092239ffaa48e3dc27
jin-co <lilijk@hanmail.net>
2022-02-04 21:41:45 +0900

    chore: update

--- AndroidApps/HungryAngry/app/src/main/java/com/example/hungryangry/MainActivity.java
+++ AndroidApps/HungryAngry/app/src/main/java/com/example/hungryangry/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.hungryangry;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
         starterCard = findViewById(R.id.card_view_starters);
         mainCard = findViewById(R.id.card_view_main);
+
+        // to move from one activity to another activity
+        starterCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startersActivityIntent = new Intent(
+                        MainActivity.this, // from
+                        StarterActivity.class); // to
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
 
