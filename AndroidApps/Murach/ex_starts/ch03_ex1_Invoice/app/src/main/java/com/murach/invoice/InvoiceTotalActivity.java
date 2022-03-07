89967880d420abb70f82a7cbd126ae8a0fbedcac
jin-co <lilijk@hanmail.net>
2022-03-07 10:04:30 -0500

    Update

--- AndroidApps/Murach/ex_starts/ch03_ex1_Invoice/app/src/main/java/com/murach/invoice/InvoiceTotalActivity.java
+++ AndroidApps/Murach/ex_starts/ch03_ex1_Invoice/app/src/main/java/com/murach/invoice/InvoiceTotalActivity.java
@@ -7,39 +7,20 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class InvoiceTotalActivity extends Activity implements TextView.OnEditorActionListener {
-	// variables
-	private EditText subtotal;
-	private double calculatedSubtotal;
-	private TextView total;
+public class InvoiceTotalActivity extends Activity {
+	// fields
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_invoice_total);
-		subtotal = (EditText) findViewById(R.id.subtotalEditText);
-		total = (TextView) findViewById(R.id.totalTextView);
-		calculatedSubtotal = Integer.parseInt(subtotal.toString());
 
-		subtotal.setOnEditorActionListener(this);
 	}
 
 	public void calculateAndDisplay() {
-		if (calculatedSubtotal >= 100) {
-			calculatedSubtotal = (calculatedSubtotal * 0.1);
-		} else if (calculatedSubtotal >= 200) {
-			calculatedSubtotal = (calculatedSubtotal * 0.2);
-		}
-		total.setText("$" + String.valueOf(calculatedSubtotal));
+
 	}
 
 
-	@Override
-	public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-		if (i == EditorInfo.IME_ACTION_DONE || i == EditorInfo.IME_ACTION_UNSPECIFIED) {
-			calculateAndDisplay();
-			total.setText("Run");
-		}
-		return false;
-	}
+
 }
