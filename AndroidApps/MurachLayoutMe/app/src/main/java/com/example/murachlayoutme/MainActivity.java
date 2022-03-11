608d3b346618cfcacfb9ff330d039aef7ffa5962
jin-co <lilijk@hanmail.net>
2022-03-11 14:37:31 -0500

    Update

--- AndroidApps/MurachLayoutMe/app/src/main/java/com/example/murachlayoutme/MainActivity.java
+++ AndroidApps/MurachLayoutMe/app/src/main/java/com/example/murachlayoutme/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.murachlayoutme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // spinner
+        spinner = (Spinner) findViewById(R.id.spinner_main);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.locations,
+                android.R.layout.simple_spinner_item
+        );
+
+        // setting resources for the spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter); // sets the array adapter for the spinner
+        spinner.setSelection(3); // sets the default selection
+        spinner.setOnItemSelectedListener(this);
+
+        // hiding text
+        /*
+        * .setVisibility(View.View.GONE) / .setVisibility(View.VISIBLE
+        * */
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getApplicationContext(), choice, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
