1dac0d42108dd1a8f3000d93f9f18a4f0d32d7f8
jin-co <lilijk@hanmail.net>
2022-03-15 17:46:02 -0400

    Update

--- AndroidApps/MidCalculator/app/src/main/java/com/example/midcalculator/MainActivity.java
+++ AndroidApps/MidCalculator/app/src/main/java/com/example/midcalculator/MainActivity.java
@@ -2,13 +2,155 @@ package com.example.midcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.view.View.OnKeyListener;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity
+        implements TextView.OnEditorActionListener, View.OnClickListener,
+        OnKeyListener, OnSeekBarChangeListener {
+    // define variables for the widgets
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button percentUpButton;
+    private Button percentDownButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private SeekBar seekBar;
+
+    // define the SharedPreferences object
+    private SharedPreferences savedValues;
+
+    // define instance variables that should be saved
+    private String billAmountString = "";
+    private float tipPercent = .15f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get references to the widgets
+        billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        seekBar = (SeekBar) findViewById(R.id.seek_bar);
+
+        // set the listeners
+        billAmountEditText.setOnEditorActionListener(this);
+
+        seekBar.setOnSeekBarChangeListener(this);
+        seekBar.setOnKeyListener(this);
+
+        // get SharedPreferences object
+        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+    }
+
+    @Override
+    public void onPause() {
+        // save the instance variables
+        SharedPreferences.Editor editor = savedValues.edit();
+        editor.putString("billAmountString", billAmountString);
+        editor.putFloat("tipPercent", tipPercent);
+        editor.commit();
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // get the instance variables
+        billAmountString = savedValues.getString("billAmountString", "");
+        tipPercent = savedValues.getFloat("tipPercent", 0.15f);
+
+        // set the bill amount on its widget
+        billAmountEditText.setText(billAmountString);
+
+        // calculate and display
+        calculateAndDisplay();
+    }
+
+    public void calculateAndDisplay() {
+
+        // get the bill amount
+        billAmountString = billAmountEditText.getText().toString();
+        float billAmount;
+        if (billAmountString.equals("")) {
+            billAmount = 0;
+        }
+        else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+
+        // calculate tip and total
+        float tipAmount = billAmount * tipPercent;
+        float totalAmount = billAmount + tipAmount;
+
+        // get tip percent
+        int progress = seekBar.getProgress();
+        tipPercent = (float) progress / 100;
+
+        // display the other results with formatting
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        tipTextView.setText(currency.format(tipAmount));
+        totalTextView.setText(currency.format(totalAmount));
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percentTextView.setText(percent.format(tipPercent));
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+            calculateAndDisplay();
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.percentDownButton:
+//                tipPercent = tipPercent - .01f;
+//                calculateAndDisplay();
+//                break;
+//            case R.id.percentUpButton:
+//                tipPercent = tipPercent + .01f;
+//                calculateAndDisplay();
+//                break;
+//        }
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        return false;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
