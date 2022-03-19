922578b908f9a89657a73000b241ab8cbaf214e4
jin-co <lilijk@hanmail.net>
2022-03-19 14:33:47 -0400

    Update

---
+++ AndroidApps/Practice/MyShopPal_Version_85/app/src/main/java/com/myshoppal/models/Cart.kt
@@ -0,0 +1,23 @@
+package com.myshoppal.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * A data model class for Cart item with required fields.
+ */
+@Parcelize
+data class Cart(
+    val user_id: String = "",
+    // TODO Step 1: Add a param as product owner.
+    // START
+    val product_owner_id: String = "",
+    // END
+    val product_id: String = "",
+    val title: String = "",
+    val price: String = "",
+    val image: String = "",
+    var cart_quantity: String = "",
+    var stock_quantity: String = "",
+    var id: String = "",
+) : Parcelable
