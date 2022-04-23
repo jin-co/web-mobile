a8486141a7459e39e46fe7e71aec05d775f21b2a
jin-co <lilijk@hanmail.net>
2022-04-23 10:33:49 -0400

    Update

--- AndroidApps/MarketingApp/app/src/main/java/com/jinco/marketingapp/MainActivity.kt
+++ AndroidApps/MarketingApp/app/src/main/java/com/jinco/marketingapp/MainActivity.kt
@@ -3,21 +3,38 @@ package com.jinco.marketingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Spinner
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private var etContactName:TextInputEditText? = null
+    private var etContactNumber:TextInputEditText? = null
+    private var etDisplayName:TextInputEditText? = null
+    private var etStartDate:TextInputEditText? = null
+    private var cbJuinor:CheckBox? = null
+    private var cbImmediatedStart:CheckBox? = null
+    private var spJobTitle:Spinner? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnPreview: Button = findViewById(R.id.btn_preview)
+        etContactName = findViewById(R.id.et_contact_name)
+        etContactNumber = findViewById(R.id.et_contact_number)
+        etDisplayName = findViewById(R.id.et_display_name)
+        etStartDate = findViewById(R.id.et_start_date)
+        cbJuinor = findViewById(R.id.cb_junior)
+        cbImmediatedStart = findViewById(R.id.cb_immediate_start)
+        spJobTitle = findViewById(R.id.spn_job_title)
+
         btnPreview.setOnClickListener {
             Toast.makeText(this, "button", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun onPreviewClicked() {
-        val etContactName:TextInputEditText = findViewById(R.id.et_contact_name)
-        val etContactNumber:TextInputEditText = findViewById(R.id.et_contact_number)
+
     }
 }
