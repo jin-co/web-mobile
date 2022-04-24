f19d02647fd1d70bcd3bfc8690401549a97b57f3
jin-co <lilijk@hanmail.net>
2022-04-24 10:11:28 -0400

    Update

--- AndroidApps/MarketingApp/app/src/main/java/com/jinco/marketingapp/MainActivity.kt
+++ AndroidApps/MarketingApp/app/src/main/java/com/jinco/marketingapp/MainActivity.kt
@@ -9,29 +9,34 @@ import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    private var etContactName:TextInputEditText? = null
-    private var etContactNumber:TextInputEditText? = null
-    private var etDisplayName:TextInputEditText? = null
-    private var etStartDate:TextInputEditText? = null
-    private var cbJuinor:CheckBox? = null
-    private var cbImmediatedStart:CheckBox? = null
-    private var spJobTitle:Spinner? = null
+//    private var etContactName:TextInputEditText? = null
+//    private var etContactNumber:TextInputEditText? = null
+//    private var etDisplayName:TextInputEditText? = null
+//    private var etStartDate:TextInputEditText? = null
+//    private var cbJuinor:CheckBox? = null
+//    private var cbImmediatedStart:CheckBox? = null
+//    private var spJobTitle:Spinner? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnPreview: Button = findViewById(R.id.btn_preview)
-        etContactName = findViewById(R.id.et_contact_name)
-        etContactNumber = findViewById(R.id.et_contact_number)
-        etDisplayName = findViewById(R.id.et_display_name)
-        etStartDate = findViewById(R.id.et_start_date)
-        cbJuinor = findViewById(R.id.cb_junior)
-        cbImmediatedStart = findViewById(R.id.cb_immediate_start)
-        spJobTitle = findViewById(R.id.spn_job_title)
+//        val btnPreview: Button = findViewById(R.id.btn_preview)
+//        etContactName = findViewById(R.id.et_contact_name)
+//        etContactNumber = findViewById(R.id.et_contact_number)
+//        etDisplayName = findViewById(R.id.et_display_name)
+//        etStartDate = findViewById(R.id.et_start_date)
+//        cbJuinor = findViewById(R.id.cb_junior)
+//        cbImmediatedStart = findViewById(R.id.cb_immediate_start)
+//        spJobTitle = findViewById(R.id.spn_job_title)
+//
+//        btnPreview.setOnClickListener {
+//            Toast.makeText(this, "button", Toast.LENGTH_LONG).show()
+//        }
 
-        btnPreview.setOnClickListener {
-            Toast.makeText(this, "button", Toast.LENGTH_LONG).show()
-        }
+
+
+
+
     }
 
     private fun onPreviewClicked() {
