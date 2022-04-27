3eca644e569a4ed773975c9e3f321b2e9f1ab2fb
jin-co <lilijk@hanmail.net>
2022-04-27 11:01:26 -0400

    Added text

--- AndroidApps/MarketingApp/app/src/main/java/com/jinco/marketingapp/PreviewActivity.kt
+++ AndroidApps/MarketingApp/app/src/main/java/com/jinco/marketingapp/PreviewActivity.kt
@@ -1,11 +1,17 @@
 package com.jinco.marketingapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
+    //    var message: Message? = null
+    //    var messagePreviewText:String? = null
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
@@ -26,17 +32,35 @@ class PreviewActivity : AppCompatActivity() {
 //                "$cbImmediateStart\n" +
 //                "$spJobTitle\n"
 
+        displayMessage()
+        setupButton()
+    }
+
+    private fun displayMessage() {
         // as -> casting
-        val message: Message = intent.getSerializableExtra("Message") as Message
-        val messagePreviewText = """
-            Hi ${message.contactName},
-            
-            My name is ${message.displayName} and I am ${message.getFullJobDescription()}
-            I have a portfolio of apps to demonstrate my technical skills.
-            I am able to start a new position ${message.getAvailability()}.
-            Please get in touch if you have any suitable roles for me.
-            Thanks and best regards.            
-        """.trimIndent()
+        message = intent.getSerializableExtra("Message") as Message
+        messagePreviewText = """
+                Hi ${message.contactName},
+                
+                My name is ${message.displayName} and I am ${message.getFullJobDescription()}
+                I have a portfolio of apps to demonstrate my technical skills.
+                I am able to start a new position ${message.getAvailability()}.
+                Please get in touch if you have any suitable roles for me.
+                Thanks and best regards.            
+            """.trimIndent()
         tv_message.text = messagePreviewText
+    }
+
+    private fun setupButton() {
+        btn_send.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto: ${message.contactNumber}")
+                putExtra("sms_body", messagePreviewText)
+            }
+//            intent.data = Uri.parse("smsto:") // This ensures only SMS apps respond
+//            intent.putExtra("sms_body", message)
+//            intent.putExtra(Intent.EXTRA_STREAM, attachment)
+            startActivity(intent)
+        }
     }
 }
