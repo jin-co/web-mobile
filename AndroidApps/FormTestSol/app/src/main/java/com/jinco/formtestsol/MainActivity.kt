2de3f9d20ea2f95ede0d52d921c2f15f7602c26c
jin-co <lilijk@hanmail.net>
2022-04-29 09:59:15 -0400

    Update

--- AndroidApps/FormTestSol/app/src/main/java/com/jinco/formtestsol/MainActivity.kt
+++ AndroidApps/FormTestSol/app/src/main/java/com/jinco/formtestsol/MainActivity.kt
@@ -2,10 +2,38 @@ package com.jinco.formtestsol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupSpinner()
+        setupButton()
+    }
+
+    private fun setupButton() {
+        val titles = arrayOf("Miss", "Mrs", "Mr", "Ms")
+        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titles)
+        spn_title.adapter = titleAdapter
+    }
+
+    private fun setupSpinner() {
+        btn_Create.setOnClickListener{
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        val user = User(
+            spn_title.selectedItem as String,
+            ti_first_name.text.toString(),
+            ti_last_name.text.toString(),
+            ti_email.text.toString(),
+            ti_phone.text.toString(),
+            ti_password.text.toString(),
+        )
     }
 }
