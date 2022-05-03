9d91c8c650d9775334ef9415ef1ee37da9870e22
jin-co <lilijk@hanmail.net>
2022-05-03 09:58:54 -0400

    Update

--- AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/MainActivity.kt
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/MainActivity.kt
@@ -3,11 +3,15 @@ package com.jinco.recordkeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jinco.recordkeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
