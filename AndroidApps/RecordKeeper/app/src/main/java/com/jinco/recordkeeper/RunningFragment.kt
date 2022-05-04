04770025871864edf1e3801916dac579ed4572c6
jin-co <lilijk@hanmail.net>
2022-05-04 10:16:48 -0400

    Update

---
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/RunningFragment.kt
@@ -0,0 +1,19 @@
+package com.jinco.recordkeeper
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RunningFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_running, container, false)
+        return view
+    }
+}
