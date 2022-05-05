87fd15800ba8faa27a2306207a793ed166db1114
jin-co <lilijk@hanmail.net>
2022-05-05 09:47:07 -0400

    Update

--- AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/MainActivity.kt
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/MainActivity.kt
@@ -2,11 +2,13 @@ package com.jinco.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jinco.recordkeeper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +21,22 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         binding.btnRun.setOnClickListener{
+            Log.e("btnRun", "run clicked")
             onRunClicked()
         }
 
         binding.btnCycle.setOnClickListener{
+            Log.e("btnCycle", "cycle clicked")
             onCyclingClicked()
         }
+
+//        binding.btnCycle.setOnClickListener(this)
+
+//        binding.btnCycle.setOnClickListener(object: View.OnClickListener {
+//            override fun onClick(p0: View?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
     }
 
     private fun onRunClicked() {
@@ -35,7 +47,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun onCyclingClicked() {
         supportFragmentManager.commit {
-            replace(R.id.fl_content, RunningFragment())
+            replace(R.id.fl_content, CyclingFragment())
         }
     }
+
+//    override fun onClick(v: View?) {
+//
+//    }
 }
