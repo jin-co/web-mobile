5443fdd2f7bea5b9cd878aa0f4f380b053eae4c0
jin-co <lilijk@hanmail.net>
2022-05-07 09:56:42 -0400

    Update

--- AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/RunningFragment.kt
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/RunningFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.jinco.recordkeeper.databinding.FragmentRunningBinding
 
 class RunningFragment:Fragment() {
-
+    private lateinit var binding: FragmentRunningBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_running, container, false)
-        return view
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+        return  binding.root
+//        val view = inflater.inflate(R.layout.fragment_running, container, false)
+//        return view
     }
 }
