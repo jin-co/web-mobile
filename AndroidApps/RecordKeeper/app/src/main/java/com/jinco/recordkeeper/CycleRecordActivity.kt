eb227caaaee9abb68fc49c2513d88d07c2a25e11
jin-co <lilijk@hanmail.net>
2022-05-07 10:35:02 -0400

    Added cycling record

---
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/CycleRecordActivity.kt
@@ -0,0 +1,16 @@
+package com.jinco.recordkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jinco.recordkeeper.databinding.ActivityCycleRecordBinding
+
+class CycleRecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCycleRecordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCycleRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val distance = intent.getStringExtra("Distance")
+    }
+}
--- AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/CyclingFragment.kt
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/CyclingFragment.kt
@@ -1,18 +1,42 @@
 package com.jinco.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.jinco.recordkeeper.databinding.FragmentCyclingBinding
 
-class CyclingFragment:Fragment() {
+class CyclingFragment : Fragment() {
+    private lateinit var binding: FragmentCyclingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cycling, container, false)
+        binding = FragmentCyclingBinding.inflate(inflater, container, false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_cycling, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.con5km.setOnClickListener { launchCycleRecordScreen("5km") }
+        binding.con10km.setOnClickListener { launchCycleRecordScreen("10km") }
+        binding.conHalf.setOnClickListener { launchCycleRecordScreen("half") }
+        binding.conFull.setOnClickListener { launchCycleRecordScreen("full") }
+    }
+
+    private fun launchCycleRecordScreen(distance: String) {
+        val intent = Intent(context, CyclingFragment::class.java)
+        intent.putExtra("Distance", distance)
+        startActivity(intent)
     }
 }
