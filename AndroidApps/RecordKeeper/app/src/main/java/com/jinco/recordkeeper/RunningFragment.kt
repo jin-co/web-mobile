49bf812ca7fa93a60e7c425597b4854d78137f70
jin-co <lilijk@hanmail.net>
2022-05-07 10:10:39 -0400

    Added running record activity

--- AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/RunningFragment.kt
+++ AndroidApps/RecordKeeper/app/src/main/java/com/jinco/recordkeeper/RunningFragment.kt
@@ -1,5 +1,6 @@
 package com.jinco.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jinco.recordkeeper.databinding.FragmentRunningBinding
 
-class RunningFragment:Fragment() {
+class RunningFragment : Fragment() {
     private lateinit var binding: FragmentRunningBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,8 +16,36 @@ class RunningFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRunningBinding.inflate(inflater, container, false)
-        return  binding.root
+        return binding.root
 //        val view = inflater.inflate(R.layout.fragment_running, container, false)
 //        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.con5km.setOnClickListener {
+            launchRunningRecordScreen()
+        }
+
+        binding.con10km.setOnClickListener {
+            launchRunningRecordScreen()
+        }
+
+        binding.conHalf.setOnClickListener {
+            launchRunningRecordScreen()
+        }
+
+        binding.conFull.setOnClickListener {
+            launchRunningRecordScreen()
+        }
+    }
+
+    private fun launchRunningRecordScreen() {
+        val intent = Intent(context, RunRecordActivity::class.java)
+        startActivity(intent)
     }
 }
