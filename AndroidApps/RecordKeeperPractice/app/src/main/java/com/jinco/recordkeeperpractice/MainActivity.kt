a2755c3c91a18c803bfebe62807cda77debe0903
jin-co <lilijk@hanmail.net>
2022-05-10 09:52:58 -0400

    Update

--- AndroidApps/RecordKeeperPractice/app/src/main/java/com/jinco/recordkeeperpractice/MainActivity.kt
+++ AndroidApps/RecordKeeperPractice/app/src/main/java/com/jinco/recordkeeperpractice/MainActivity.kt
@@ -2,10 +2,33 @@ package com.jinco.recordkeeperpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.jinco.recordkeeperpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSubmit.setOnClickListener { submitForm() }
+    }
+
+    private fun submitForm() {
+        var contactName = binding.etContactName.text
+        var contactNumber = binding.etContactNumber.text
+        var displayName = binding.etDisplayName.text
+        var startDate = binding.etStartDate.text
+        var junior = binding.cbJunior.text
+        var job = binding.spJob.selectedItem
+        var immediate = binding.cbImmediate.text
+
+        var sentence = "Hi, I am $contactName call me $displayName and" +
+                "contact me $contactNumber I am a $junior $job" +
+                "I can start $immediate or $startDate"
+        Toast.makeText(this, sentence, Toast.LENGTH_LONG).show()
     }
 }
