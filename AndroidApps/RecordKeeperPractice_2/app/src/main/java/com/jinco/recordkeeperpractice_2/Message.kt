455d95e6d6c473f05bf96db0920aed6ae3717281
jin-co <lilijk@hanmail.net>
2022-05-13 09:33:50 -0400

    Update

--- AndroidApps/RecordKeeperPractice_2/app/src/main/java/com/jinco/recordkeeperpractice_2/Message.kt
+++ AndroidApps/RecordKeeperPractice_2/app/src/main/java/com/jinco/recordkeeperpractice_2/Message.kt
@@ -1,5 +1,9 @@
 package com.jinco.recordkeeperpractice_2
 
 data class Message (
-    val contactName:String
+    val contactName:String,
+    val contactNumber:String,
+    val displayName:String,
+    val job:String,
+    val immediate:Boolean,
 )
