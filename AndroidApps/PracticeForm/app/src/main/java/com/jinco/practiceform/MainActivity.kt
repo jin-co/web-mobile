1431094724e74c6fd516f6f0f27722e582818be9
jin-co <lilijk@hanmail.net>
2022-05-15 10:33:18 -0400

    Added class

--- AndroidApps/PracticeForm/app/src/main/java/com/jinco/practiceform/MainActivity.kt
+++ AndroidApps/PracticeForm/app/src/main/java/com/jinco/practiceform/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.ArrayAdapter
 import com.jinco.practiceform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -14,11 +14,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         setSpinner()
+        setupButton()
+    }
+
+    private fun setupButton() {
+        val user = User(
+            firstName = binding.etFirstName.toString(),
+            lastName = binding.etLastName.toString(),
+            title = binding.spTitle.selectedItem.toString(),
+            password = binding.etPassword.toString(),
+            email = binding.etEmail.toString(),
+            phone = binding.etPhone.toString()
+        )
     }
 
     private fun setSpinner() {
         val titleValue = arrayOf("", "", "")
-        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titleValue)
+        val titleAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titleValue)
         binding.spTitle.adapter = titleAdapter
     }
 }
---
+++ AndroidApps/PracticeForm/app/src/main/java/com/jinco/practiceform/User.kt
@@ -0,0 +1,10 @@
+package com.jinco.practiceform
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val title: String,
+    val password: String,
+    val email: String,
+    val phone: String
+)
