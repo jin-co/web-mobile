3b69cf7b7f7a788d99601be21aa2cc99127d5c35
jin-co <lilijk@hanmail.net>
2022-05-15 10:25:04 -0400

    Added spinner

--- AndroidApps/PracticeForm/app/src/main/java/com/jinco/practiceform/MainActivity.kt
+++ AndroidApps/PracticeForm/app/src/main/java/com/jinco/practiceform/MainActivity.kt
@@ -2,10 +2,23 @@ package com.jinco.practiceform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.jinco.practiceform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSpinner()
+    }
+
+    private fun setSpinner() {
+        val titleValue = arrayOf("", "", "")
+        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titleValue)
+        binding.spTitle.adapter = titleAdapter
     }
 }
