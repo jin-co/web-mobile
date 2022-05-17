30465562a7ad91514a1841d6635428d8105e7853
jin-co <lilijk@hanmail.net>
2022-05-17 10:03:13 -0400

    Update

--- AndroidApps/PracticeFrom2/app/src/main/java/com/jinco/practicefrom2/SummaryActivity.kt
+++ AndroidApps/PracticeFrom2/app/src/main/java/com/jinco/practicefrom2/SummaryActivity.kt
@@ -1,7 +1,10 @@
 package com.jinco.practicefrom2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import com.jinco.practicefrom2.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
@@ -15,7 +18,21 @@ class SummaryActivity : AppCompatActivity() {
         setupSummary()
     }
 
+    private fun setupButton() {
+        binding.btnSend.setOnClickListener {
+            sendMessage()
+        }
+    }
+
+    private fun sendMessage() {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto: ee")
+        startActivity(intent)
+    }
+
     private fun setupSummary() {
-        TODO("Not yet implemented")
+        val user = intent.getSerializableExtra("user") as User
+        binding.tvSummary.text = "${user.name} ${user.phone} ${user.what} ${user.what}"
+        setupButton()
     }
 }
