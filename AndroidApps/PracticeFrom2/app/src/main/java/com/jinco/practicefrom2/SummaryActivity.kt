5e92196af57dbba3d22442522425dc9634e68681
jin-co <lilijk@hanmail.net>
2022-05-17 09:46:34 -0400

    Added summary layout

--- AndroidApps/PracticeFrom2/app/src/main/java/com/jinco/practicefrom2/SummaryActivity.kt
+++ AndroidApps/PracticeFrom2/app/src/main/java/com/jinco/practicefrom2/SummaryActivity.kt
@@ -2,10 +2,20 @@ package com.jinco.practicefrom2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jinco.practicefrom2.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySummaryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_summary)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupSummary()
+    }
+
+    private fun setupSummary() {
+        TODO("Not yet implemented")
     }
 }
