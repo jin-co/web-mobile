7c81e428190fda4328f51018a3c27550692f493e
jin-co <lilijk@hanmail.net>
2022-05-18 09:15:31 -0400

    Update

--- AndroidApps/ENamecard/app/src/main/java/com/jinco/enamecard/MainActivity.kt
+++ AndroidApps/ENamecard/app/src/main/java/com/jinco/enamecard/MainActivity.kt
@@ -2,10 +2,22 @@ package com.jinco.enamecard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.jinco.enamecard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSpinner()
+    }
+
+    private fun setSpinner() {
+        val jopSpinner = arrayOf("one", "two")
+        val jopAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, jopSpinner)
+        binding.spJob.adapter = jopAdapter
     }
 }
