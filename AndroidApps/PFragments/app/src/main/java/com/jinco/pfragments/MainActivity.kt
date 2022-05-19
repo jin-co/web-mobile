c027af868e19f19fcfb28f5305aa92ffb596b00e
jin-co <lilijk@hanmail.net>
2022-05-19 09:41:30 -0400

    Update

--- AndroidApps/PFragments/app/src/main/java/com/jinco/pfragments/MainActivity.kt
+++ AndroidApps/PFragments/app/src/main/java/com/jinco/pfragments/MainActivity.kt
@@ -2,10 +2,23 @@ package com.jinco.pfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.jinco.pfragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSpinner()
+    }
+
+    private fun setSpinner() {
+        val titleSpinner = arrayOf("1", "2")
+        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titleSpinner)
+        binding.spWhat.adapter = titleAdapter
     }
 }
