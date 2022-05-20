5b9bd5b59b10d96fd55e40d93ecc12fecd830825
jin-co <lilijk@hanmail.net>
2022-05-20 09:19:59 -0400

    Update

--- AndroidApps/PFragments/app/src/main/java/com/jinco/pfragments/MainActivity.kt
+++ AndroidApps/PFragments/app/src/main/java/com/jinco/pfragments/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jinco.pfragments
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import androidx.fragment.app.commit
 import com.jinco.pfragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,14 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        supportFragmentManager.commit {
+            add(R.id.frame, RunFragment())
+        }
+
         setSpinner()
     }
 
     private fun setSpinner() {
-        val titleSpinner = arrayOf("1", "2")
-        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titleSpinner)
-        binding.spWhat.adapter = titleAdapter
+
     }
 }
--- AndroidApps/PFragments/app/src/main/java/com/jinco/pfragments/RunFragment.kt
+++ AndroidApps/PFragments/app/src/main/java/com/jinco/pfragments/RunFragment.kt
@@ -12,6 +12,6 @@ class RunFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_run, container, false)
     }
 }
