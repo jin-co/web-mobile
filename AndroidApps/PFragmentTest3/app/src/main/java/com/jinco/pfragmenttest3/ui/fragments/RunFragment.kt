6f15adf36b2bf8289edf69fefc4c17b13c9e8922
jin-co <lilijk@hanmail.net>
2022-05-24 09:30:26 -0400

    Finished

---
+++ AndroidApps/PFragmentTest3/app/src/main/java/com/jinco/pfragmenttest3/ui/fragments/RunFragment.kt
@@ -0,0 +1,18 @@
+package com.jinco.pfragmenttest3.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jinco.pfragmenttest3.R
+
+class RunFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_run, container, false)
+    }
+}
