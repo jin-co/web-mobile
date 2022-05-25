cc1e838f59204ed56cb44a654b0a6ac8ff69bf86
jin-co <lilijk@hanmail.net>
2022-05-25 09:32:26 -0400

    Update

--- AndroidApps/PFragment/app/src/main/java/com/jinco/pfragment/MainActivity.kt
+++ AndroidApps/PFragment/app/src/main/java/com/jinco/pfragment/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jinco.pfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.jinco.pfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            add(R.id.frame, RunFragment())
+        }
     }
 }
