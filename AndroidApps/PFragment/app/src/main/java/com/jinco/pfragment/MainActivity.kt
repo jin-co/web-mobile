f2bbf347fb1ba7df0876a430ef33d382deaafdf2
jin-co <lilijk@hanmail.net>
2022-05-26 09:32:06 -0400

    Update

--- AndroidApps/PFragment/app/src/main/java/com/jinco/pfragment/MainActivity.kt
+++ AndroidApps/PFragment/app/src/main/java/com/jinco/pfragment/MainActivity.kt
@@ -2,10 +2,12 @@ package com.jinco.pfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jinco.pfragment.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemReselectedListener {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +16,20 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.commit {
             add(R.id.frame, RunFragment())
+        }
+
+        binding.bottomNav.setOnNavigationItemReselectedListener (this)
+    }
+
+    override fun onNavigationItemReselected(item: MenuItem) {
+        if(item.itemId == R.id.run) {
+            supportFragmentManager.commit {
+                replace(R.id.frame, RunFragment())
+            }
+        } else {
+            supportFragmentManager.commit {
+                replace(R.id.frame, CycleFragment())
+            }
         }
     }
 }
