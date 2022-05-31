53e4fba31e9c4db338fe989d1c857546d94a199c
jin-co <lilijk@hanmail.net>
2022-05-31 09:37:32 -0400

    Update

--- AndroidApps/PFragementTest/app/src/main/java/com/jinco/pfragementtest/MainActivity.kt
+++ AndroidApps/PFragementTest/app/src/main/java/com/jinco/pfragementtest/MainActivity.kt
@@ -2,10 +2,12 @@ package com.jinco.pfragementtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
 import com.jinco.pfragementtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +17,24 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.commit {
             add(R.id.frame, RunFragment())
         }
+
+        binding.bottomNav.setOnItemSelectedListener (this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.run) {
+            supportFragmentManager.commit {
+                replace(R.id.frame, RunFragment())
+            }
+            return true
+        }
+
+        if (item.itemId == R.id.cycle) {
+            supportFragmentManager.commit {
+                replace(R.id.frame, RunFragment())
+            }
+            return true
+        }
+        return false
     }
 }
