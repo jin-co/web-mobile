edaf71f48096d609908c3d8712ea1f686560e07e
jin-co <lilijk@hanmail.net>
2022-06-04 14:23:01 -0400

    Update

--- React/p1-react/src/components/ExpenseItem.js
+++ React/p1-react/src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   const expDate = new Date(2000, 2, 22);
   console.log(expDate)
   const title = 'Title'
   const amount = 22
+
+  // const month = props.date.toLocaleString('en-US', {month:'long'})
+  // const year = props.date.toLocaleString('en-US', {day:'2-digit'})
+  // const day = props.date.getFullYear()
   return (
     // <div className="expense-item">
     //   <div> { expDate.toISOString() } </div>
@@ -13,7 +18,9 @@ function ExpenseItem(props) {
     //   </div>
     // </div>
     <div className="expense-item">
-      <div> { props.date.toISOString() } </div>
+      <div> { props.date.toISOString() } 
+        <ExpenseDate date={props.date}></ExpenseDate>
+      </div>
       <div className="expense-item__description">
         <h2> { props.title } </h2>
         <div className="expense-item__price">${ props.amount }</div>
