f4f0918ef44748865a0fecb33a701663e91c078b
jin-co <lilijk@hanmail.net>
2022-06-05 10:53:16 -0400

    Added state

--- React/p1-react/src/components/Expenses/ExpenseItem.js
+++ React/p1-react/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card'
+import Card from "../UI/Card";
+import { useState } from "react";
+
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title)
+
   const expDate = new Date(2000, 2, 22);
-  console.log(expDate)
-  const title = 'Title'
-  const amount = 22
+  console.log(expDate);
+  
+  const amount = 22;
 
   //event
   const clickHandler = () => {
-    console.log("second way")
-  }
+    setTitle('updated')
+    console.log("second way");
+  };
 
   // const month = props.date.toLocaleString('en-US', {month:'long'})
   // const year = props.date.toLocaleString('en-US', {day:'2-digit'})
@@ -24,15 +29,24 @@ function ExpenseItem(props) {
     //   </div>
     // </div>
     <Card className="expense-item">
-      <div> { props.date.toISOString() } 
+      <div>
+        {" "}
+        {props.date.toISOString()}
         <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
-        <h2> { props.title } </h2>
-        <div className="expense-item__price">${ props.amount }</div>
+        {/* <h2> {props.title} </h2> */}
+        <h2> {title} </h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={() => console.log('çlicked')}>Click</button>
-      <button onClick={function() {console.log('çlicked')}}>Click</button>
+      <button onClick={() => console.log("çlicked")}>Click</button>
+      <button
+        onClick={function () {
+          console.log("çlicked");
+        }}
+      >
+        Click
+      </button>
       <button onClick={clickHandler}>Click</button>
     </Card>
   );
