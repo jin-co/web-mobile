ed48859a185a70bedb6104a1dcef8421a3cba13a
jin-co <lilijk@hanmail.net>
2022-06-12 09:45:11 +0900

    Completed

--- AndroidApps/PFragmentTest4/app/src/main/java/com/jinco/pfragmenttest4/MainActivity.kt
+++ AndroidApps/PFragmentTest4/app/src/main/java/com/jinco/pfragmenttest4/MainActivity.kt
@@ -2,10 +2,33 @@ package com.jinco.pfragmenttest4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
+import com.jinco.pfragmenttest4.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.commit {
+            add(R.id.run, RunFragment())
+        }
+
+        binding.bottomNav.setOnItemSelectedListener (this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.run) {
+            supportFragmentManager.commit {
+                replace(R.id.run, RunFragment())
+            }
+        } else {
+            supportFragmentManager.commit {
+                replace(R.id.cycle, CycleFragment())
+            }
+        }
     }
 }
