b1323ee77019162d745155410061e91c6faf7f75
jin-co <lilijk@hanmail.net>
2022-06-12 09:57:25 +0900

    Update

--- AndroidApps/PFragmentTest4/app/src/main/java/com/jinco/pfragmenttest4/MainActivity.kt
+++ AndroidApps/PFragmentTest4/app/src/main/java/com/jinco/pfragmenttest4/MainActivity.kt
@@ -7,28 +7,28 @@ import androidx.fragment.app.commit
 import com.google.android.material.navigation.NavigationBarView
 import com.jinco.pfragmenttest4.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportFragmentManager.commit {
-            add(R.id.run, RunFragment())
+            add(R.id.fragment, RunFragment(R.layout.fragment_run))
         }
 
-        binding.bottomNav.setOnItemSelectedListener (this)
+
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.run) {
-            supportFragmentManager.commit {
-                replace(R.id.run, RunFragment())
-            }
-        } else {
-            supportFragmentManager.commit {
-                replace(R.id.cycle, CycleFragment())
-            }
-        }
-    }
+//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//        if (item.itemId == R.id.fragment) {
+//            supportFragmentManager.commit {
+//                replace(R.id.fragment, RunFragment(R.layout.fragment_run))
+//            }
+//        } else {
+//            supportFragmentManager.commit {
+//                replace(R.id.fragment, CycleFragment(R.layout.fragment_cycle))
+//            }
+//        }
+//    }
 }
