34313b4a6527a6a8f1e23e0ae98eed22ae0f7828
jin-co <lilijk@hanmail.net>
2022-06-14 05:48:40 +0900

    Added menu

--- AndroidApps/PFragementTest3/app/src/main/java/com/jinco/pfragementtest3/MainActivity.kt
+++ AndroidApps/PFragementTest3/app/src/main/java/com/jinco/pfragementtest3/MainActivity.kt
@@ -2,10 +2,29 @@ package com.jinco.pfragementtest3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationBarView
+import com.jinco.pfragementtest3.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.run) {
+            Toast.makeText(this, R.id.run.toString(), Toast.LENGTH_SHORT).show()
+            return true
+        } else {
+            Log.e("haha", "ahah")
+            return true
+        }
     }
 }
