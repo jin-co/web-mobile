70e91667563252e585269c6916cff6c092bd69bc
jin-co <lilijk@hanmail.net>
2022-06-15 08:56:22 +0900

    Completed test

--- AndroidApps/PFragmentTest42/app/src/main/java/com/jinco/pfragmenttest4/MainActivity.kt
+++ AndroidApps/PFragmentTest42/app/src/main/java/com/jinco/pfragmenttest4/MainActivity.kt
@@ -2,10 +2,27 @@ package com.jinco.pfragmenttest4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
+import com.jinco.pfragmenttest4.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            add(R.id.frame, RunFragment())
+        }
+
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
