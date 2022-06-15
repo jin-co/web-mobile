9d29cc2e71af491d1a70c76fbb0359c1c428db3f
jin-co <lilijk@hanmail.net>
2022-06-16 07:16:38 +0900

    Added run fragment

--- AndroidApps/PFragmentTest1/app/src/main/java/com/jinco/pfragmenttest1/MainActivity.kt
+++ AndroidApps/PFragmentTest1/app/src/main/java/com/jinco/pfragmenttest1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.jinco.pfragmenttest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.jinco.pfragmenttest1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            add(R.id.frame, RunFragment())
+        }
+
+
     }
 }
