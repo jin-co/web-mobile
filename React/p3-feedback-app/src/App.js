4656ba1098e1fc91d394da419d1bc0759fb02370
jin-co <lilijk@hanmail.net>
2022-06-17 12:48:11 +0900

    Update

--- React/p3-feedback-app/src/App.js
+++ React/p3-feedback-app/src/App.js
@@ -1,44 +1,59 @@
+import Header from "./components/Header"
+
+
 function App() {
-  const title = 'Blog post'
-  const body = 'body'
-  const comments = [
-    { id: 1, text: 'comment 1' },
-    { id: 2, text: 'comment 2' },
-    { id: 3, text: 'comment 3' },
-    { id: 4, text: 'comment 4' },
-  ]
+  /** practice*/
+  // const title = 'Blog post'
+  // const body = 'body'
+  // const comments = [
+  //   { id: 1, text: 'comment 1' },
+  //   { id: 2, text: 'comment 2' },
+  //   { id: 3, text: 'comment 3' },
+  //   { id: 4, text: 'comment 4' },
+  // ]
 
-  const loading = false
-  const showComments = false
-  const commentBlock = (
-    <div className="comment">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((c, idx) => (
-          <li key={idx}>c.text</li>
-        ))}
-      </ul>
-    </div>
-  )
+  // const loading = false
+  // const showComments = false
+  // const commentBlock = (
+  //   <div className="comment">
+  //     <h3>Comments ({comments.length})</h3>
+  //     <ul>
+  //       {comments.map((c, idx) => (
+  //         <li key={idx}>c.text</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // )
+  
+  // if (loading) return <h1>Loading...</h1>
+  /** practice*/
 
-  if (loading) return <h1>Loading...</h1>
+  const styles = {backgroundColor: 'blue'}
+
   return (
-    <div>
-      <h1>Hi</h1>
-      {/* {showComments ? (
-        <div className="comment">
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((c, idx) => (
-              <li key={idx}>c.text</li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        null
-      )} */}
-      {showComments && commentBlock}
-    </div>
+    // <div>
+    //   <h1>Hi</h1>
+    //   {/* {showComments ? (
+    //     <div className="comment">
+    //       <h3>Comments ({comments.length})</h3>
+    //       <ul>
+    //         {comments.map((c, idx) => (
+    //           <li key={idx}>c.text</li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   ) : (
+    //     null
+    //   )} */}
+    //   {showComments && commentBlock}
+    // </div>
+
+    <>
+      <Header text={'Hello'}/>
+      <div className="container">
+        <h1 style={styles}>App</h1>
+      </div>
+    </>
   )
 }
 
