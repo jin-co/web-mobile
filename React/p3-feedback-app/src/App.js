62c3c11a0e49ec43f090ce357aeb6f8f4cf5c176
jin-co <lilijk@hanmail.net>
2022-06-17 11:11:45 +0900

    Completed test

--- React/p3-feedback-app/src/App.js
+++ React/p3-feedback-app/src/App.js
@@ -1,7 +1,43 @@
 function App() {
-  return(
+  const title = 'Blog post'
+  const body = 'body'
+  const comments = [
+    { id: 1, text: 'comment 1' },
+    { id: 2, text: 'comment 2' },
+    { id: 3, text: 'comment 3' },
+    { id: 4, text: 'comment 4' },
+  ]
+
+  const loading = false
+  const showComments = false
+  const commentBlock = (
+    <div className="comment">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((c, idx) => (
+          <li key={idx}>c.text</li>
+        ))}
+      </ul>
+    </div>
+  )
+
+  if (loading) return <h1>Loading...</h1>
+  return (
     <div>
       <h1>Hi</h1>
+      {/* {showComments ? (
+        <div className="comment">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((c, idx) => (
+              <li key={idx}>c.text</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        null
+      )} */}
+      {showComments && commentBlock}
     </div>
   )
 }
--- React/p3-feedback-app/src/index.js
+++ React/p3-feedback-app/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 )
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
