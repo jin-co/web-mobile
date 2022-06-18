da61d6ff3b7144232e8e674022588c6231d171b6
jin-co <lilijk@hanmail.net>
2022-06-19 08:53:51 +0900

    Update

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/SplashActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.jinco.ecommerceapp.UI.activites
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.jinco.ecommerceapp.MainActivity
 import com.jinco.ecommerceapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            },
+            2500
+        )
     }
 }
