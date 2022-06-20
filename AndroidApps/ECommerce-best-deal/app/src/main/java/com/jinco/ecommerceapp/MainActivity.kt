2a4a2544dcf1e735217a77ff24ebff8acc0d8832
jin-co <lilijk@hanmail.net>
2022-06-21 08:13:22 +0900

    Update

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
@@ -2,18 +2,13 @@ package com.jinco.ecommerceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Handle the splash screen transition.
-        val splashScreen = installSplashScreen()
-
         super.onCreate(savedInstanceState)
-        Thread.sleep(2000)
         setContentView(R.layout.activity_main)
     }
 }
--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/SplashActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/SplashActivity.kt
@@ -12,6 +12,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        @Suppress("DEPRECATION")
         Handler().postDelayed(
             {
                 startActivity(Intent(this@SplashActivity, MainActivity::class.java))
