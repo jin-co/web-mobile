abf8e3e89361ba891410edbd50fd6b90036568da
jin-co <lilijk@hanmail.net>
2022-06-20 12:41:46 +0900

    Update

--- React/test-feedback/src/App.js
+++ React/test-feedback/src/App.js
@@ -1,8 +1,21 @@
+import Header from "./components/Header"
+
 const App = () => {
-  return(
-    <div>
-      h
-    </div>
+  const items = [
+    { id: 1, text: 'text 1' },
+    { id: 2, text: 'text 2' },
+    { id: 3, text: 'text 3' },
+    { id: 4, text: 'text 4' },
+  ]
+  return (
+    <>
+      <Header text = 'Hello' />
+      <div className="container">    
+        {items.map((item) => (
+          <h4 key={item.id}>{item.text}</h4>
+        ))}
+      </div>
+    </>
   )
 }
 
