eefdd3164fda498410e192833a4780a68e410bcc
jin-co <lilijk@hanmail.net>
2022-06-22 07:28:24 +0900

    init best deal

---
+++ AndroidApps/Jincobestdealapp/app/src/main/java/com/jinco/jin_co_best_deal_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jinco.jin_co_best_deal_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
