6b166c8adde692ab5fb473aa2aa75318d101ef0f
jin-co <lilijk@hanmail.net>
2022-06-23 08:09:12 +0900

    Completed widget text test

---
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/widgets/BoldTextTest.kt
@@ -0,0 +1,21 @@
+package com.jinco.ecommerceapp.UI.widgets
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class BoldTextTest(context: Context, attributeSet: AttributeSet) :
+    AppCompatTextView(context, attributeSet) {
+        init {
+            addText()
+        }
+
+    private fun addText() {
+        val boldTypeface: Typeface = Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        typeface = boldTypeface
+    }
+//    val boldTypeface: Typeface =
+//        Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+//    typeface = boldTypeface
+}
