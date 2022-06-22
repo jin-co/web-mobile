3638fbdd0b0905ec75037538babcfe6d7aee8f2b
jin-co <lilijk@hanmail.net>
2022-06-23 08:04:28 +0900

    Created bold text view

---
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/widgets/BoldTextView.kt
@@ -0,0 +1,20 @@
+package com.jinco.ecommerceapp.UI.widgets
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class BoldTextView(context: Context, attributeSet: AttributeSet) :
+    AppCompatTextView(context, attributeSet) {
+
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val boldTypeface: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        typeface = boldTypeface
+    }
+}
