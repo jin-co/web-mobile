6e7b126f379ccf9d3be44b34e5e1c3daff8c55f4
jin-co <lilijk@hanmail.net>
2022-06-22 13:01:16 +0900

    Added validation

--- React/test-feedback2/src/components/FeedbackForm.js
+++ React/test-feedback2/src/components/FeedbackForm.js
@@ -5,11 +5,21 @@ import Button from './shared/Button'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
-  // const [text, setText] = useState('')
-  // const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextInput = (e) => {
     setText(e.target.value)
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage('')
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Over 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
     console.log(text)
   }
 
@@ -18,9 +28,10 @@ const FeedbackForm = () => {
       <form>
         <h2>Survey</h2>
         <div className="input-group">
-          <input type="text" placeholder='Review' onChange={handleTextInput} />
-          <Button>Send</Button>
+          <input type="text" placeholder="Review" onChange={handleTextInput} />
+          <Button isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
