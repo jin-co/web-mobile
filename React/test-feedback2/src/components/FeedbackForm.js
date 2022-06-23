d408c31b54575dbf292955d9f35095ef2e666483
jin-co <lilijk@hanmail.net>
2022-06-23 14:02:43 +0900

    Update

--- React/test-feedback2/src/components/FeedbackForm.js
+++ React/test-feedback2/src/components/FeedbackForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from './shared/Card'
 import { useState } from 'react'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
@@ -10,10 +11,10 @@ const FeedbackForm = () => {
   const [isDisabled, setIsDisabled] = useState(true)
 
   const handelTextInput = (e) => {
-    if(text ==='') {
+    if (text === '') {
       setIsDisabled(true)
       setMessage(null)
-    } else if(text.trim().length <= 10) {
+    } else if (text.trim().length <= 10) {
       setIsDisabled(true)
       setMessage('invalid')
     } else {
@@ -21,18 +22,21 @@ const FeedbackForm = () => {
       setMessage('')
     }
 
-    setText(e.target.value)    
-  }  
+    setText(e.target.value)
+  }
 
   return (
     <Card>
       <form action="">
         <h2>How would you rate?</h2>
+        <RatingSelect />
         <div className="input-group">
           <input type="text" placeholder="Write" onChange={handelTextInput} />
-          <Button type="submit" isDisabled={isDisabled}>Send</Button>
+          <Button type="submit" isDisabled={isDisabled}>
+            Send
+          </Button>
         </div>
-        {message&&<div className='message'>{message}</div>}
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
