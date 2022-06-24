e29b77ff3d61895d9c98c1c597f213e2dc824ee0
jin-co <lilijk@hanmail.net>
2022-06-24 11:48:28 +0900

    Completed array.from test

--- React/test-feedback2/src/components/RatingSelect.js
+++ React/test-feedback2/src/components/RatingSelect.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
 
-const RatingSelect = () => {
-  const [selected, setSelected] = useState(10)
-  const handleChange = () => {}
+const RatingSelect = ({ select }) => {
+  const [selected, setSelected] = useState(select)
+  const handleChange = (e) => {
+    setSelected(+e.currentTarget.value)
+    // select(selected)
+    console.log(selected)
+  }
   return (
-    <ul>
-      {Array.from({ length: 10 }, (_, i) => {
-        ;<li key={`rating-${i + 1}`}>
+    <ul className="rating">
+      {Array.from({ length: 10 }, (_, i) => (
+        <li className="rating">
           <input
             type="radio"
-            id={`num${i + 1}`}
             name="rating"
+            id={`rating-${i + 1}`}
             value={i + 1}
             onChange={handleChange}
             checked={selected === i + 1}
           />
-          <label htmlFor={`num${i + 1}`}>{i + 1}</label>
+          <label htmlFor={`rating-${i + 1}`}>{i + 1}</label>
         </li>
-      })}
+      ))}
     </ul>
   )
 }
