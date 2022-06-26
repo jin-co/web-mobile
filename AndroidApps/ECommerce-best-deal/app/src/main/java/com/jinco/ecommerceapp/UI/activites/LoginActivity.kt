4bd2dd14d6af937675d7f8d1716b09f2dcfed071
jin-co <lilijk@hanmail.net>
2022-06-27 08:37:08 +0900

    Update

---
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.jinco.ecommerceapp.UI.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jinco.ecommerceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView()
+    }
+}
