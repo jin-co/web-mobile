53a5bb1cb657228ce0c6b116c251fa561efe70c7
jin-co <lilijk@hanmail.net>
2022-06-28 08:14:52 +0900

    added login intent

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.jinco.ecommerceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.jinco.ecommerceapp.UI.activites.LoginActivity
+import com.jinco.ecommerceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         val splashScreen = installSplashScreen()
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.tvMainLogin.setOnContextClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
