9b804e6c765ba805ba88a822548198ea8a384e2b
jin-co <lilijk@hanmail.net>
2022-06-30 08:00:26 +0900

    Added base activity

---
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
@@ -0,0 +1,38 @@
+package com.jinco.ecommerceapp.UI.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.jinco.ecommerceapp.R
+import com.jinco.ecommerceapp.databinding.ActivityBaseBinding
+
+class BaseActivity : AppCompatActivity() {
+    lateinit var binding: ActivityBaseBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun shwoErrorSnackBar(message:String, errorMessage:Boolean) {
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if(errorMessage) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarError
+                )
+            )
+        } else {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+        }
+    }
+}
