e90410302f675b97a13bf9f761937ca290c39823
jin-co <lilijk@hanmail.net>
2022-07-04 08:52:09 +0900

    Added fragment

--- AndroidApps/Testgeneral/app/src/main/java/com/jinco/testgeneral/MainActivity.kt
+++ AndroidApps/Testgeneral/app/src/main/java/com/jinco/testgeneral/MainActivity.kt
@@ -2,10 +2,29 @@ package com.jinco.testgeneral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationBarView
+import com.jinco.testgeneral.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.run) {
+            supportFragmentManager.commit {
+                add(R.id.frame, RunFragment())
+            }
+            return true
+        } else {
+            return true
+        }
     }
 }
