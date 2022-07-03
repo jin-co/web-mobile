a0e6f5c03cc2f365b8c4c1b6aed6035316589674
jin-co <lilijk@hanmail.net>
2022-07-04 08:29:51 +0900

    init test

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
@@ -6,29 +6,25 @@ import com.google.android.material.snackbar.Snackbar
 import com.jinco.ecommerceapp.R
 
 open class BaseActivity : AppCompatActivity() {
-    val snackbar = Snackbar.make(findViewById(android.R.id.content), "ha", Snackbar.LENGTH_LONG)
-    val snackbarView = snackbar.view
-    
+    fun showErrorSnackBar(message:String, errorMessage:Boolean) {
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
 
-//    fun showErrorSnackBar(message:String, errorMessage:Boolean) {
-//        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
-//        val snackBarView = snackBar.view
-//
-//        if(errorMessage) {
-//            snackBarView.setBackgroundColor(
-//                ContextCompat.getColor(
-//                    this@BaseActivity,
-//                    R.color.colorSnackBarError
-//                )
-//            )
-//        } else {
-//            snackBarView.setBackgroundColor(
-//                ContextCompat.getColor(
-//                    this@BaseActivity,
-//                    R.color.colorSnackBarSuccess
-//                )
-//            )
-//        }
-//        snackBar.show()
-//    }
+        if(errorMessage) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarError
+                )
+            )
+        } else {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+        }
+        snackBar.show()
+    }
 }
