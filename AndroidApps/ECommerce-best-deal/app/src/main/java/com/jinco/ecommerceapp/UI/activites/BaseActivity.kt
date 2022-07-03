320c01832248bb00a412276fa42192ef2ec86b36
jin-co <lilijk@hanmail.net>
2022-07-04 08:22:17 +0900

    init snack bar test

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
@@ -6,25 +6,27 @@ import com.google.android.material.snackbar.Snackbar
 import com.jinco.ecommerceapp.R
 
 open class BaseActivity : AppCompatActivity() {
-    fun showErrorSnackBar(message:String, errorMessage:Boolean) {
-        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
-        val snackBarView = snackBar.view
 
-        if(errorMessage) {
-            snackBarView.setBackgroundColor(
-                ContextCompat.getColor(
-                    this@BaseActivity,
-                    R.color.colorSnackBarError
-                )
-            )
-        } else {
-            snackBarView.setBackgroundColor(
-                ContextCompat.getColor(
-                    this@BaseActivity,
-                    R.color.colorSnackBarSuccess
-                )
-            )
-        }
-        snackBar.show()
-    }
+    
+//    fun showErrorSnackBar(message:String, errorMessage:Boolean) {
+//        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+//        val snackBarView = snackBar.view
+//
+//        if(errorMessage) {
+//            snackBarView.setBackgroundColor(
+//                ContextCompat.getColor(
+//                    this@BaseActivity,
+//                    R.color.colorSnackBarError
+//                )
+//            )
+//        } else {
+//            snackBarView.setBackgroundColor(
+//                ContextCompat.getColor(
+//                    this@BaseActivity,
+//                    R.color.colorSnackBarSuccess
+//                )
+//            )
+//        }
+//        snackBar.show()
+//    }
 }
