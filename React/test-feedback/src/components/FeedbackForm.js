fd6be54fdcd8413be0da37d9d0d44309bbdde807
jin-co <lilijk@hanmail.net>
2022-07-04 13:12:20 +0900

    Setting

---
+++ React/test-feedback/src/components/FeedbackForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FeedbackForm = () => {
+  return (
+    <div>FeedbackForm</div>
+  )
+}
---
+++ React/test-feedback/src/components/FeedbackSelect.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FeedbackSelect = () => {
+  return (
+    <div>FeedbackSelect</div>
+  )
+}
