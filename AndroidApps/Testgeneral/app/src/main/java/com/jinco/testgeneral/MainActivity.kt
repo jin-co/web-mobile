3d7326321ad3fa12877d4272a8a6428e545637e5
jin-co <lilijk@hanmail.net>
2022-07-05 08:18:52 +0900

    Added splash screen

--- AndroidApps/Testgeneral/app/src/main/java/com/jinco/testgeneral/MainActivity.kt
+++ AndroidApps/Testgeneral/app/src/main/java/com/jinco/testgeneral/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jinco.testgeneral
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.commit
 import com.google.android.material.navigation.NavigationBarView
 import com.jinco.testgeneral.databinding.ActivityMainBinding
@@ -10,6 +11,7 @@ import com.jinco.testgeneral.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
