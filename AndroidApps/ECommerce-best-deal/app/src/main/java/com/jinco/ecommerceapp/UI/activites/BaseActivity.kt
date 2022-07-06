7643098746b44054044c1da05120eb04db399e17
jin-co <lilijk@hanmail.net>
2022-07-07 08:22:32 +0900

    Update

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/BaseActivity.kt
@@ -1,16 +1,30 @@
 package com.jinco.ecommerceapp.UI.activites
 
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.snackbar.Snackbar
 import com.jinco.ecommerceapp.R
+import com.jinco.ecommerceapp.databinding.ActivityMainBinding
 
 open class BaseActivity : AppCompatActivity() {
-    fun showErrorSnackBar(message:String, errorMessage:Boolean) {
-        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+    lateinit var progressDialog: Dialog
+    lateinit var binding: ActivityBase
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityBase.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
         val snackBarView = snackBar.view
 
-        if(errorMessage) {
+        if (errorMessage) {
             snackBarView.setBackgroundColor(
                 ContextCompat.getColor(
                     this@BaseActivity,
@@ -35,6 +49,12 @@ open class BaseActivity : AppCompatActivity() {
 //            actionBar.setDisplayHomeAsUpEnabled(true)
 //            actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_background)
 //        }
+
+    }
+
+    fun showSpinner(text: String) {
+        progressDialog = Dialog(this)
+        progressDialog.setContentView(R.layout.dialog_spinner)
 
     }
 }
