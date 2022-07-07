214d7f1a6b7b6c90c9892dcffca53190ed3bd32f
jin-co <lilijk@hanmail.net>
2022-07-07 11:12:37 +0900

    Added rating select

--- React/t-feedback/src/component/RatingSelect.js
+++ React/t-feedback/src/component/RatingSelect.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 
 const RatingSelect = () => {
+  const [selected, setSelected] = useState(10)
+  const handleClick = (e) => {
+    console.log(e.target.value)
+    setSelected(+e.target.value)
+  }
+
   return (
-    <div>RatingSelect</div>
+    <ul className="rating">
+      {Array.from({ length: 10 }, (_, i) => (
+        <li key={i}>
+          <input
+            type="radio"
+            id={`rating-${i + 1}`}
+            value={i + 1}
+            onClick={handleClick}
+            checked={selected === i + 1}
+          />
+          <label htmlFor={`rating-${i + 1}`}>{i + 1}</label>
+        </li>
+      ))}
+    </ul>
   )
 }
 
