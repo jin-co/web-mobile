488c108d4ebb31115fd60284505d1c9d7bfade2d
jin-co <lilijk@hanmail.net>
2022-07-11 12:49:01 +0900

    Added nav

--- React/p4-github-finder/src/App.js
+++ React/p4-github-finder/src/App.js
@@ -1,6 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+
 const App = () => {
   return (
-    <div></div>
+    <Router>
+      <div>
+        <h1>hh</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="flex flex-col justify-between h-screen">
+                  <Navbar />
+
+                  <main>Content</main>
+                </div>
+              </>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
