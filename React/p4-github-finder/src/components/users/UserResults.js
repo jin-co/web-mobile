8a332401f86baca2f4e85dc5990aa7dba156c9e7
jin-co <lilijk@hanmail.net>
2022-07-11 13:40:37 +0900

    Added user item

--- React/p4-github-finder/src/components/users/UserResults.js
+++ React/p4-github-finder/src/components/users/UserResults.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 const UserResults = () => {
   const [users, setUsers] = useState([])
@@ -20,8 +21,8 @@ const UserResults = () => {
   if(!isLoading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user) => (
-          <h3>{user.login}</h3>
+        {users.map((user) => (          
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
