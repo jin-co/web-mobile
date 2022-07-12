a870530cd36e747011295a8a2a4b812c8d52d352
jin-co <lilijk@hanmail.net>
2022-07-13 08:59:55 +0900

    Added forgot id activity

---
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/ForgotIDActivity.kt
@@ -0,0 +1,12 @@
+package com.jinco.ecommerceapp.UI.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jinco.ecommerceapp.R
+
+class ForgotIDActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_id)
+    }
+}
