df91b1fc355f7cc50a20cfd14ef58d1ee171344b
jin-co <lilijk@hanmail.net>
2022-07-12 10:32:50 +0900

    Added comps

--- React/test-feed/src/App.js
+++ React/test-feed/src/App.js
@@ -1,3 +1,10 @@
+import Header from "./components/shared/Header"
+import FeedbackData from './data/FeedbackData'
+import { useState } from "react"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
+
 const App = () => {
   return (
     <div>
