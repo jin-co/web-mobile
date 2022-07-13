075d8955337c44ba1f8254c04518dabbc565f2ee
jin-co <lilijk@hanmail.net>
2022-07-13 09:06:15 +0900

    Update

--- AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/ForgotPasswordActivity.kt
+++ AndroidApps/ECommerce-best-deal/app/src/main/java/com/jinco/ecommerceapp/UI/activites/ForgotPasswordActivity.kt
@@ -1,12 +1,21 @@
 package com.jinco.ecommerceapp.UI.activites
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jinco.ecommerceapp.R
+import com.jinco.ecommerceapp.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    lateinit var binding: ActivityForgotPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvFotgotPasswordToFindIdPage.setOnClickListener {
+            val intent = Intent(this@ForgotPasswordActivity, ForgotIDActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
