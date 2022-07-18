b3900780913058c1c97e8ab81d083b50e5a7459d
jin-co <lilijk@hanmail.net>
2022-07-18 11:47:16 +0900

    Added footer

--- React/p4-github-finder-redo/src/App.js
+++ React/p4-github-finder-redo/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
           <Route />
         </Routes>
       </main>
+      <Footer />
     </Router>
   )
 }
