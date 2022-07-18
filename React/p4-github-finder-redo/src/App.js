536a546a121ea8a9fb659eb49fed274a0d8376ed
jin-co <lilijk@hanmail.net>
2022-07-18 12:42:24 +0900

    Added route

--- React/p4-github-finder-redo/src/App.js
+++ React/p4-github-finder-redo/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Notfound from './pages/Notfound'
 
 const App = () => {
   return (
@@ -9,9 +12,12 @@ const App = () => {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
       </div>
-      <main className='container mx-auto px-3 pb-12'>
+      <main className="container mx-auto px-3 pb-12">
         <Routes>
-          <Route />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/notfound" element={<Notfound />} />
+          <Route path="/*" element={<Notfound />} />
         </Routes>
       </main>
       <Footer />
