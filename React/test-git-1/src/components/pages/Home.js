efc9ca0f057f10b8597d17c1c8eabf9e2c96c005
jin-co <lilijk@hanmail.net>
2022-07-19 11:36:20 +0900

    Completed test

--- React/test-git-1/src/components/pages/Home.js
+++ React/test-git-1/src/components/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import UserList from '../users/UserList'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className="text-6xl">Welcome</h1>
+      <UserList />
+    </div>
   )
 }
 
--- React/test-git-1/src/components/users/UserList.js
+++ React/test-git-1/src/components/users/UserList.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import { useEffect, useState } from 'react'
 
 const UserList = () => {
-  return (
-    <div>UserList</div>
-  )
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetchUsers()
+    console.log(users)
+  }, [])
+
+  const fetchUsers = async () => {
+    const res = await fetch('https://api.github.com/users')
+    const data = await res.json()
+    setUsers(data)
+    setLoading(false)
+  }
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2">
+        {users.map((u) => (
+          <UserItem key={u.id} user={u} />
+        ))}
+      </div>
+    )
+  } else {
+    return <Spinner />
+  }
 }
 
 export default UserList
