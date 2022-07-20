a4cdd8cdbd50aaa8265afee49d71d12888c38372
jin-co <lilijk@hanmail.net>
2022-07-20 13:04:00 +0900

    Update

--- React/p4-github-finder-redo/src/components/users/UserSearch.js
+++ React/p4-github-finder-redo/src/components/users/UserSearch.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContext'
+import AlertContext from '../../context/alert/AlertContext'
 
 const UserSearch = () => {
   const { users, searchUsers, clearUser} = useContext(GithubContext)
+  const { setAlert } = useContext(AlertContext)
   const [text, setText] = useState('')
 
   const handleChange = (e) => {
@@ -14,7 +16,7 @@ const UserSearch = () => {
     e.preventDefault()
 
     if (text === '') {
-      //todo -> alert
+      setAlert('Enter', 'error')
     } else {
       searchUsers(text)
       setText('')
--- React/p4-github-finder-redo/src/context/alert/AlertReducer.js
+++ React/p4-github-finder-redo/src/context/alert/AlertReducer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const alertReducer = () => {
-  return (
-    <div>AlertReducer</div>
-  )
+const alertReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_ALERT':
+      return action.payload
+
+    case 'DELETE_ALERT':
+      return null
+
+    default:
+      return state
+  }
 }
 
 export default alertReducer
