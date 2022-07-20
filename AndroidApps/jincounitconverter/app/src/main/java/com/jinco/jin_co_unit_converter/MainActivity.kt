31bed16cdb57f2bb02e3c5b3f33a9b418b733d76
jin-co <lilijk@hanmail.net>
2022-07-20 09:07:06 +0900

    Added spinner

--- AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/MainActivity.kt
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/MainActivity.kt
@@ -2,14 +2,26 @@ package com.jinco.jin_co_unit_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.jinco.jin_co_unit_converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         val splashScreen = installSplashScreen()
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setUpSpinner()
+    }
+
+    private fun setUpSpinner() {
+        val units = arrayOf("feet", "meter", "cm", "feet")
+        val unitAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, units)
+        binding.spUnitFrom.adapter = unitAdapter
+        binding.spUnitTo.adapter = unitAdapter
     }
 }
