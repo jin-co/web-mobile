78f3c0da0d2649bba39849744e454724f007741f
jin-co <lilijk@hanmail.net>
2022-07-20 13:14:39 +0900

    Added error comp

--- React/p4-github-finder-redo/src/App.js
+++ React/p4-github-finder-redo/src/App.js
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Notfound from './pages/Notfound'
 import { GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
+import Alert from './components/layout/Alert'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
