be555064b744a966453afd8ea51969dcf52690ea
jin-co <lilijk@hanmail.net>
2022-07-22 10:43:59 +0900

    Added comps

--- React/test-feedback/src/App.js
+++ React/test-feedback/src/App.js
@@ -1,3 +1,7 @@
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
+import Header from './components/shared/Header'
+
 const App = () => {
   return (
     <div></div>
