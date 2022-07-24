046aea3d14450fa9e8a33f9acd1597ff2888b61b
jin-co <lilijk@hanmail.net>
2022-07-24 16:27:11 +0900

    Added comps

---
+++ Angular/jin-co-foodshop/src/app/components/recipies/recipy-list/recipy-item/recipy-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipy-item',
+  templateUrl: './recipy-item.component.html',
+  styleUrls: ['./recipy-item.component.css']
+})
+export class RecipyItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular/jin-co-foodshop/src/app/components/recipies/recipy-list/recipy-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipy-list',
+  templateUrl: './recipy-list.component.html',
+  styleUrls: ['./recipy-list.component.css']
+})
+export class RecipyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
