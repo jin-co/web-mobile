7f437fd43e0c737cf3321991b56cc6cf3d529c4a
jin-co <lilijk@hanmail.net>
2022-07-25 17:33:16 +0900

    Added login

--- React/p6-house-market/src/pages/Signup.js
+++ React/p6-house-market/src/pages/Signup.js
@@ -1,8 +1,86 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const { name, email, password } = formData
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Signup</div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome</p>
+        </header>
+        <main>
+          <form action="">
+            <input
+              type="text"
+              className="emailInput"
+              placeholder="Name"
+              id="name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+
+              <img
+                src={visibilityIcon}
+                alt=""
+                className="showPassword"
+                onClick={() => setShowPassword((prev) => !prev)}
+              />
+            </div>
+
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password
+            </Link>
+            <div className="signupBar">
+              <p className="singInText">Sign In</p>
+              <button className="signupButton">
+                <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+          //todo -> google auth
+          <Link to="/sign-up" className="registerLink">
+            Sign Up Instead
+          </Link>
+        </main>
+      </div>
+    </>
   )
 }
 
