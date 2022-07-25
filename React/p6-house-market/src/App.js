4aa05658721eef91c6596a33ec83d49c63deb208
jin-co <lilijk@hanmail.net>
2022-07-25 13:35:37 +0900

    Update

--- React/p6-house-market/src/App.js
+++ React/p6-house-market/src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Authors from './pages/Authors'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import ForgotPassword from './pages/ForgotPassword'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/profile" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
