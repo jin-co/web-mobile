3358d87319d34a3d3f557b0e592424210f4b3c5e
jin-co <lilijk@hanmail.net>
2022-07-27 19:26:15 +0900

    Update

--- React/test-feedback/src/App.js
+++ React/test-feedback/src/App.js
@@ -2,12 +2,17 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import Header from './components/shared/Header'
 import FeedbackForm from './components/FeedbackForm'
+import FeedbackStats from './components/FeedbackStats'
+import { useState } from 'react'
 
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData)
   return (
     <>
       <Header/>
       <main className='container'>
+      
+      <FeedbackForm/>
         <FeedbackList/>
       </main>
     </>
