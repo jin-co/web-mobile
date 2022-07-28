f4e1c73696b91be1ded5c15620383c71fcc8997c
jin-co <lilijk@hanmail.net>
2022-07-28 10:18:50 +0900

    Update

--- React/test-feedback/src/components/FeedbackForm.js
+++ React/test-feedback/src/components/FeedbackForm.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import { useState } from 'react'
+import SelectRating from './SelectRating'
 
 const FeedbackForm = () => {
+  const [text, setText] = useState('')
+  const [message, setMessage] = useState('')
+  const [isDisabled, setIsDisabled] = useState(true)
+  const [rating, setRating] = useState(0)
+
+  const handleChange = (e) => {
+    if (e.target.value.length > 10) {
+      setIsDisabled(false)
+      setMessage('')
+    } else {
+      setIsDisabled(true)
+      setMessage('Over 10')
+    }
+  }
+
   return (
-    <div>FeedbackForm</div>
+    <Card>
+      <SelectRating />
+      <form action="">
+        <input type="text" onChange={handleChange} />
+        <Button isDisabled={isDisabled}>Add</Button>
+      </form>
+      {message}
+    </Card>
   )
 }
 
