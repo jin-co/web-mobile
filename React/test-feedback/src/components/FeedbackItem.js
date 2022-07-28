73bc70fd59897d1cb89ac73e8a9d3fc0f82a1457
jin-co <lilijk@hanmail.net>
2022-07-28 10:32:01 +0900

    Added icon

--- React/test-feedback/src/components/FeedbackItem.js
+++ React/test-feedback/src/components/FeedbackItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Card from './shared/Card'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 
-const FeedbackItem = ({item}) => {
+const FeedbackItem = ({ item }) => {
   return (
     <Card>
-    <p className='text-display'>{item.text}</p>
-    <p className='rating-display'>{item.rating}</p>    
+      <p className="text-display">{item.text}</p>
+      <FaTimes className="close" color="red" />
+      <FaEdit className="edit" color="purple" />
+      <p className="rating-display">{item.rating}</p>
     </Card>
   )
 }
