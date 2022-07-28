1edeb6bb64596d63d27978119ab79cbbd0c6ae5d
jin-co <lilijk@hanmail.net>
2022-07-28 10:39:57 +0900

    Added animation

--- React/test-feedback/src/components/FeedbackList.js
+++ React/test-feedback/src/components/FeedbackList.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
+import { AnimatePresence as Animate, motion } from 'framer-motion'
 
 const FeedbackList = ({ feedback, handleDelete }) => {
   return (
     <>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-      ))}
-    </>
+    <Animate>
+        {feedback.map((item) => (
+          <motion.div key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+    </Animate>
+      </>
   )
 }
 
