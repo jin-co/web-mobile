2b0265c21e8b93214a387bbe552a12bfbabd3e66
jin-co <lilijk@hanmail.net>
2022-07-28 10:37:13 +0900

    Added delete

--- React/test-feedback/src/components/FeedbackItem.js
+++ React/test-feedback/src/components/FeedbackItem.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Card from './shared/Card'
 import { FaTimes, FaEdit } from 'react-icons/fa'
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
+  
+
   return (
     <Card>
       <p className="text-display">{item.text}</p>
-      <FaTimes className="close" color="red" />
+      <FaTimes
+        className="close"
+        color="red"
+        onClick={() => handleDelete(item.id)}
+      />
       <FaEdit className="edit" color="purple" />
       <p className="rating-display">{item.rating}</p>
     </Card>
