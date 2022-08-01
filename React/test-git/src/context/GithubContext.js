ed662473f8772661589107f81c2f3165f9d346e1
jin-co <lilijk@hanmail.net>
2022-08-01 16:27:39 +0900

    Update

--- React/test-git/src/context/GithubContext.js
+++ React/test-git/src/context/GithubContext.js
@@ -1,27 +1,34 @@
 import React from 'react'
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect, useReducer } from 'react'
+import GithubReducer from './GithubReducer'
 
 const URL = 'https://api.github.com/'
 
 const GithubContext = createContext()
 
 export const GithubProvider = (props) => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)  
+  const initialState = {
+    users: [],
+    loading: true,
+  }
+  const [state, dispatch] = useReducer(GithubReducer, initialState)
+  const [loading, setLoading] = useState(true)
 
   const fetchUsers = async () => {
     const res = await fetch(URL + 'users')
     const data = await res.json()
-    setUsers(data)
-    setLoading(false)
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    })
   }
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
-        fetchUsers
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
       }}
     >
       {props.children}
