a776d3c977f840d08421987d23b0dba944ab9774
jin-co <lilijk@hanmail.net>
2022-08-02 21:16:17 +0900

    Added route

--- React/test-git/src/App.js
+++ React/test-git/src/App.js
@@ -1,6 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Notfound from './pages/Notfound'
+import Header from './component/layout/Header'
+import Footer from './component/layout/Footer'
+
 const App = () => {
   return (
-    <div></div>
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/notfound" element={<Notfound />} />
+          <Route path="/*" element={<Notfound />} />
+        </Routes>
+        <Footer />
+      </>
+    </Router>
   )
 }
 
