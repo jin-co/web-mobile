167950557898982dcb586dac84989ea0689b721a
jin-co <lilijk@hanmail.net>
2022-08-08 10:20:35 +0900

    Added area class

---
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Area.kt
@@ -0,0 +1,11 @@
+package com.jinco.jin_co_unit_converter.models
+
+class Area {
+    val squareMillimeter:Double = 0.0
+    val squareCentimeter:Double = 0.0
+    val squareMeter:Double = 0.0
+    val squareInch:Double = 0.0
+    val squareFoot:Double = 0.0
+    val squareYard:Double = 0.0
+    val squareMile:Double = 0.0
+}
--- AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Length.kt
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Length.kt
@@ -1,10 +1,10 @@
 package com.jinco.jin_co_unit_converter.models
 
 class Length {
-    val milliMeter: Double = 0.0
-    val centiMeter: Double = 0.0
+    val millimeter: Double = 0.0
+    val centimeter: Double = 0.0
     val meter: Double = 0.0
-    val kiloMeter: Double = 0.0
+    val kilometer: Double = 0.0
     val inch: Double = 0.0
     val foot: Double = 0.0
     val yard: Double = 0.0
