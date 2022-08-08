07c0a4838a51d84f4e94e6e84e4bde5a2984d831
jin-co <lilijk@hanmail.net>
2022-08-08 10:14:37 +0900

    Added length class

---
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Length.kt
@@ -0,0 +1,12 @@
+package com.jinco.jin_co_unit_converter.models
+
+class Length {
+    val milliMeter: Double = 0.0
+    val centiMeter: Double = 0.0
+    val meter: Double = 0.0
+    val kiloMeter: Double = 0.0
+    val inch: Double = 0.0
+    val foot: Double = 0.0
+    val yard: Double = 0.0
+    val mile: Double = 0.0
+}
