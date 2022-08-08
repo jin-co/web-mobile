a1705717a01916f3d332b4ed96e1ef8ea235ed1e
jin-co <lilijk@hanmail.net>
2022-08-08 10:16:42 +0900

    Added temprature class

---
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Temprature.kt
@@ -0,0 +1,8 @@
+package com.jinco.jin_co_unit_converter.models
+
+class Temprature {
+    val celsius:Double = 0.0
+    val fahrenheit:Double = 0.0
+    val kelvin:Double = 0.0
+    val rankine:Double = 0.0
+}
