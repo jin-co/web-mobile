afb43d7f308927157ecb002270b8139214d91c35
jin-co <lilijk@hanmail.net>
2022-08-09 10:26:58 +0900

    Added time class

--- AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Area.kt
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Area.kt
@@ -7,5 +7,10 @@ class Area {
     val squareInch:Double = 0.0
     val squareFoot:Double = 0.0
     val squareYard:Double = 0.0
-    val squareMile:Double = 0.0
+    val squareRod:Double = 0.0
+    val Hectare:Double = 0.0
+    val are:Double = 0.0
+    val acre:Double = 0.0
+    val rood:Double = 0.0
+    val barn:Double = 0.0
 }
---
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Time.kt
@@ -0,0 +1,12 @@
+package com.jinco.jin_co_unit_converter.models
+
+class Time {
+    val milliSecond:Double = 0.0
+    val second:Double = 0.0
+    val minute:Double = 0.0
+    val hour:Double = 0.0
+    val day:Double = 0.0
+    val week:Double = 0.0
+    val month:Double = 0.0
+    val year:Double = 0.0
+}
