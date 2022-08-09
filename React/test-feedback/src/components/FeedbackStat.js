7547b7d244e758f4bfb6eb486101917abef6dd3a
jin-co <lilijk@hanmail.net>
2022-08-09 14:50:02 +0900

    Update

--- React/test-feedback/src/components/FeedbackStat.js
+++ React/test-feedback/src/components/FeedbackStat.js
@@ -5,7 +5,14 @@ const FeedbackStat = ({ feedback }) => {
     feedback.reduce((arr, cur) => {
       return arr + cur.rating
     }, 0) / feedback.length
-  return <div>FeedbackStat</div>
+  average = average.toFixed(1)
+
+  return (
+    <div>
+      <p>Average: {isNaN(average) ? 0 : average}</p>
+      <p>Feedback: {feedback.length}</p>
+    </div>
+  )
 }
 
 export default FeedbackStat
