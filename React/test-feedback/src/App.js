efef457a971a3ad23b905d59ed515041c37c2e02
jin-co <lilijk@hanmail.net>
2022-08-09 14:43:33 +0900

    Added comps

--- React/test-feedback/src/App.js
+++ React/test-feedback/src/App.js
@@ -1,6 +1,24 @@
+import FeedbackList from './components/FeedbackList'
+import FeedbackForm from './components/FeedbackForm'
+import FeedbackStat from './components/FeedbackStat'
+import FeedbackData from './data/FeedbackData'
+import Header from './components/shared/Header'
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { FaQuestion } from 'react-icons/fa'
+
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
-    <div></div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStat />
+        <FeedbackForm />
+        <FeedbackList />
+      </div>
+    </>
   )
 }
 
---
+++ React/test-feedback/src/components/FeedbackStat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedbackStat = () => {
+  return (
+    <div>FeedbackStat</div>
+  )
+}
+
+export default FeedbackStat
