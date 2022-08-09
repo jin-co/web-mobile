45daeeca6e58ade3c6eb8d2403bb1bfed60ef688
jin-co <lilijk@hanmail.net>
2022-08-09 10:35:47 +0900

    Added volume

---
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Volume.kt
@@ -0,0 +1,41 @@
+package com.jinco.jin_co_unit_converter.models
+
+class Volume {
+    val USOunce: Double = 0.0
+    val USTablespoon: Double = 0.0
+    val USTeaspoon: Double = 0.0
+    val USGill: Double = 0.0
+    val USBeerBarrel: Double = 0.0
+    val USBarrel: Double = 0.0
+    val OilBarrel: Double = 0.0
+    val imperialBushel: Double = 0.0
+    val imperialPeck: Double = 0.0
+    val imperialGallon: Double = 0.0
+    val imperialQuart: Double = 0.0
+    val imperialPint: Double = 0.0
+    val imperialOunce: Double = 0.0
+    val imperialTsp: Double = 0.0
+    val nanoliter: Double = 0.0
+    val microliter: Double = 0.0
+    val milliliter: Double = 0.0
+    val centiliter: Double = 0.0
+    val deciliter: Double = 0.0
+    val liter: Double = 0.0
+    val decaliter: Double = 0.0
+    val hectoliter: Double = 0.0
+    val kiloliter: Double = 0.0
+    val megaliter: Double = 0.0
+    val cubicNanometer: Double = 0.0
+    val cubicMillimeter: Double = 0.0
+    val cubicCentimeter: Double = 0.0
+    val cubicDecimeter: Double = 0.0
+    val cubicMeter: Double = 0.0
+    val cubicKillometer: Double = 0.0
+    val cubicFoot: Double = 0.0
+    val cubicInch: Double = 0.0
+    val cubicYard: Double = 0.0
+    val cubicMile: Double = 0.0
+    val cubicRod: Double = 0.0
+    val cord: Double = 0.0
+    val hogshead: Double = 0.0
+}
