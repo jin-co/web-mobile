c933fa53ef57f6b03aedb473a2e1600b8e478771
jin-co <lilijk@hanmail.net>
2022-08-09 10:36:16 +0900

    Update

--- AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Area.kt
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Area.kt
@@ -1,16 +1,16 @@
 package com.jinco.jin_co_unit_converter.models
 
 class Area {
-    val squareMillimeter:Double = 0.0
-    val squareCentimeter:Double = 0.0
-    val squareMeter:Double = 0.0
-    val squareInch:Double = 0.0
-    val squareFoot:Double = 0.0
-    val squareYard:Double = 0.0
-    val squareRod:Double = 0.0
-    val Hectare:Double = 0.0
-    val are:Double = 0.0
-    val acre:Double = 0.0
-    val rood:Double = 0.0
-    val barn:Double = 0.0
+    val squareMillimeter: Double = 0.0
+    val squareCentimeter: Double = 0.0
+    val squareMeter: Double = 0.0
+    val squareInch: Double = 0.0
+    val squareFoot: Double = 0.0
+    val squareYard: Double = 0.0
+    val squareRod: Double = 0.0
+    val Hectare: Double = 0.0
+    val are: Double = 0.0
+    val acre: Double = 0.0
+    val rood: Double = 0.0
+    val barn: Double = 0.0
 }
