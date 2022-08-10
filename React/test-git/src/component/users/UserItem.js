def9b2951575d5c1d7c4a5724aee92c74993020e
jin-co <lilijk@hanmail.net>
2022-08-10 13:03:40 +0900

    Update

--- React/test-git/src/component/users/UserItem.js
+++ React/test-git/src/component/users/UserItem.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const UserItem = ({user}) => {
+const UserItem = ({ user: { login, avatar_url } }) => {
   return (
-    <div>
-    {user.login}
+    <div className="card compact side bg-base-100">
+      <div className="flex-row times-center space-x-4 card-body">
+        <div className="avatar">
+          <div className="rounded-full shadow w-14 h14">
+            <img className="" src={avatar_url} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="card-title">{login}</h2>
+        <Link to={`/user/${login}`}>Visit</Link>
+      </div>      
     </div>
   )
 }
