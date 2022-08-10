dbefae24763da0f1014accd8a845cc0a51c906e4
jin-co <lilijk@hanmail.net>
2022-08-10 10:22:11 +0900

    Added weight class

---
+++ AndroidApps/jincounitconverter/app/src/main/java/com/jinco/jin_co_unit_converter/models/Weight.kt
@@ -0,0 +1,33 @@
+package com.jinco.jin_co_unit_converter.models
+
+class Weight {
+    val kilogram: Double = 0.0
+    val gram: Double = 0.0
+    val decigram: Double = 0.0
+    val centigram: Double = 0.0
+    val milligram: Double = 0.0
+    val microgram: Double = 0.0
+    val manogram: Double = 0.0
+    val picogram: Double = 0.0
+    val femtogram: Double = 0.0
+    val dekagram: Double = 0.0
+    val hectogram: Double = 0.0
+    val megagram: Double = 0.0
+    val metricton: Double = 0.0
+    val longton: Double = 0.0
+    val shortton: Double = 0.0
+    val imperialton: Double = 0.0
+    val metricquintal: Double = 0.0
+    val USquintal: Double = 0.0
+    val frenchquintal: Double = 0.0
+    val stone: Double = 0.0
+    val pound: Double = 0.0
+    val ounce: Double = 0.0
+    val troyounce: Double = 0.0
+    val slug: Double = 0.0
+    val tola: Double = 0.0
+    val dram: Double = 0.0
+    val carat: Double = 0.0
+    val grain: Double = 0.0
+    val atomicmassunit: Double = 0.0
+}
