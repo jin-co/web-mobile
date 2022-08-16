d57711542e1b8a0228cc85ef3bf3061302c35f47
jin-co <lilijk@hanmail.net>
2022-08-16 20:16:52 +0900

    Added route

--- React/test-git/src/App.js
+++ React/test-git/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>App</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   )
--- React/test-git/src/components/layout/Navbar.js
+++ React/test-git/src/components/layout/Navbar.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline text-3xl" />
+          <Link to="/">Finder</Link>
+        </div>
 
-    <div>
-    </div>
+        <div className="flex-1">
+          <div className="flex">
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/notfound'></Link>
+            <Link to='/*'></Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
