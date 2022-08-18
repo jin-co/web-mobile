13b0278af468b8da9ec91aa616b8b4c38bfa7336
jin-co <lilijk@hanmail.net>
2022-08-18 15:29:31 +0900

    Update

--- React/test-gith/src/context/github/GithubContext.js
+++ React/test-gith/src/context/github/GithubContext.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { createContext, useState, useEffect } from 'react'
 
-const GithubContext = () => {
+const GithubContext = createContext()
+
+const URL = 'https://api.github.com/'
+
+export const GithubProvider = (props) => {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const fetchUsers = async () => {
+    setLoading(true)
+    const res = await fetch(URL + 'users', {
+      method: 'GET',
+    })
+    const data = await res.json()
+    setUsers(data)
+    setLoading(false)
+  }
+
   return (
-    <div>GithubContext</div>
+    <GithubContext.Provider
+      value={{
+        users,
+        loading,
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
   )
 }
 
