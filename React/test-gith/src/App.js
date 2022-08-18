904189119b3c0562238f14867b9db664aa55cfd5
jin-co <lilijk@hanmail.net>
2022-08-18 14:59:18 +0900

    Added comps

--- React/test-gith/src/App.js
+++ React/test-gith/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './component/layout/Navbar'
+import Footer from './component/layout/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
