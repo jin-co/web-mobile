6625218f8c17e0177ec272c5b97b0a61cfa628b3
jin-co <lilijk@hanmail.net>
2022-08-18 15:00:53 +0900

    Added routes

--- React/test-gith/src/component/layout/Navbar.js
+++ React/test-gith/src/component/layout/Navbar.js
@@ -3,7 +3,10 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+    </div>
   )
 }
 
