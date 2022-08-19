8d6b595361fdc5aceae79f9c3f1c320b1b1c6f52
jin-co <lilijk@hanmail.net>
2022-08-19 16:13:42 +0900

    Added spinner

--- React/p6-house-market/src/components/PrivateRoute.js
+++ React/p6-house-market/src/components/PrivateRoute.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 
 const PrivateRoute = () => {
   const { logged, statusCheck } = useAuthStatus()
 
   if (statusCheck) {
-    return <h3>spinner</h3>
+    return <Spinner />
   }
 
   return logged ? <Outlet /> : <Navigate to="/sign-in" />
--- React/p6-house-market/src/hooks/useAuthStatus.js
+++ React/p6-house-market/src/hooks/useAuthStatus.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export const useAuthStatus = () => {
   const [logged, setLogged] = useState(false)
   const [statusCheck, setStatusCheck] = useState(true)
+  const isMounted = useRef(true)
 
   useEffect(() => {
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLogged(true)
+    if (isMounted) {
+      const auth = getAuth()
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLogged(true)
+        }
         setStatusCheck(false)
-      }
-    })
-  }, [])
+      })
+    }
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
 
   return { logged, statusCheck }
 }
-
