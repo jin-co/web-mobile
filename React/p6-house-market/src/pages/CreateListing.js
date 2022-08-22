adc4eebec52cebad21d2b23f6e89dcbf65f97617
jin-co <lilijk@hanmail.net>
2022-08-22 20:28:05 +0900

    Update

--- React/p6-house-market/src/pages/CreateListing.js
+++ React/p6-house-market/src/pages/CreateListing.js
@@ -4,6 +4,14 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import { toast } from 'react-toastify'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage'
+import { db } from '../firebase.config'
+import { v4 as uuid } from 'uuid'
 
 const CreateListing = () => {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true)
@@ -45,6 +53,7 @@ const CreateListing = () => {
   const isMounted = useRef(true)
 
   useEffect(() => {
+    console.log(process.env.REACT_APP_TEST)
     if (isMounted) {
       onAuthStateChanged(auth, (user) => {
         if (user) {
@@ -101,6 +110,44 @@ const CreateListing = () => {
       geolocation.lat = latitude
       geolocation.lng = longitude
       location = address
+    }
+
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage()
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuid()}`
+        const storageRef = ref(storage, 'images/' + fileName)
+        const uploadTask = uploadBytesResumable(storageRef, image)
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break
+              case 'running':
+                console.log('Upload is running')
+                break
+            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log('File available at', downloadURL)
+            })
+          }
+        )
+      })
     }
 
     setLoading(false)
