0e125d013754aa445c6ea089a9e5d42a66de454c
jin-co <lilijk@hanmail.net>
2022-09-04 16:33:39 +0900

    Added sign up

--- React/test-housing/src/pages/SignUp.js
+++ React/test-housing/src/pages/SignUp.js
@@ -1,8 +1,90 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+  const { name, email, password } = formData
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value
+    }))
+  }
+
   return (
-    <div>SignUp</div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome</p>
+          <main>
+            <form>
+              <input
+                type="text"
+                className="nameInput"
+                placeholder="name"
+                id="name"
+                value={name}
+                onChange={handleChange}
+              />
+
+              <input
+                type="email"
+                className="emailInput"
+                placeholder="email"
+                id="email"
+                value={email}
+                onChange={handleChange}
+              />
+
+              <div className="passwordInputDiv">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  className="passwordInput"
+                  placeholder="Password"
+                  id="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+
+                <img
+                  src={visibilityIcon}
+                  alt=""
+                  className="showPassword"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                />
+              </div>
+
+              <Link to="/forgot-password" className="forgotPasswordLink">
+                Forgot Password
+              </Link>
+
+              <div className="signUpBar">
+                <p className="signUpText">Sign Up</p>
+
+                <button className="signUpButton">
+                  <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+                </button>
+              </div>
+            </form>
+
+            {/* todo */}
+
+            <Link to="/sing-in" className="registerLink">
+              Sing In Instead
+            </Link>
+          </main>
+        </header>
+      </div>
+    </>
   )
 }
 
