244c3859e79b12767cce902bb19a1cf3633ea86e
jin-co <lilijk@hanmail.net>
2022-09-04 16:26:02 +0900

    Update

--- React/test-housing/src/pages/SignIn.js
+++ React/test-housing/src/pages/SignIn.js
@@ -1,8 +1,75 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData
+
+  const handleChange = () => {}
+
   return (
-    <div>SignIn</div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome</p>
+          <main>
+            <form>
+              <input
+                type="email"
+                className="emailInput"
+                placeholder="email"
+                id="email"
+                value={email}
+                onChange={handleChange}
+              />
+
+              <div className="passwordInputDiv">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  className="passwordInput"
+                  placeholder="Password"
+                  id="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+
+                <img
+                  src={visibilityIcon}
+                  alt=""
+                  className="showPassword"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                />
+              </div>
+
+              <Link to="/forgot-password" className="forgotPasswordLink">
+                Forgot Password
+              </Link>
+
+              <div className="signInBar">
+                <p className="signInText">Sign In</p>
+
+                <button className="signInButton">
+                  <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+                </button>
+              </div>
+            </form>
+
+            {/* todo */}
+
+            <Link to='/sing-up' className='registerLink'>
+              Sing Up Instead
+            </Link>
+          </main>
+        </header>
+      </div>
+    </>
   )
 }
 
