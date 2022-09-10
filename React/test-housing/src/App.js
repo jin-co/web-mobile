09fbbef5a92b396468ff30e6a47dd0e7a2cc2176
jin-co <lilijk@hanmail.net>
2022-09-10 10:34:41 +0900

    Added category

--- React/test-housing/src/App.js
+++ React/test-housing/src/App.js
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import PrivateRoute from './components/PrivateRoute'
+import Category from './pages/Category'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />          
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
