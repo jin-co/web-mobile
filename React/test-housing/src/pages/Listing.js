e0e4b337f6b0971d3d02f281bf7e31afb1bf3b4e
jin-co <lilijk@hanmail.net>
2022-09-13 21:11:40 +0900

    Update

--- React/test-housing/src/pages/Listing.js
+++ React/test-housing/src/pages/Listing.js
@@ -6,6 +6,7 @@ import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+import { list } from 'firebase/storage'
 
 const Listing = () => {
   const [listing, setListing] = useState(null)
@@ -26,7 +27,11 @@ const Listing = () => {
         setLoading(false)
       }
     }
+    fetchListing()
   }, [])
+  if (loading) {
+    return <Spinner />
+  }
   return (
     <main>
       {/* todo */}
@@ -44,6 +49,51 @@ const Listing = () => {
         <img src={shareIcon} alt="" />
       </div>
       {shareLinkCopied && <p className="linkCopied">Link Copied</p>}
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1 ? `${listing.bedrooms}bedrooms` : '1 bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms}bathrooms`
+              : '1 bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+        {/* todo */}
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className="primaryButton"
+          >
+            Contact landlord
+          </Link>
+        )}
+      </div>
     </main>
   )
 }
