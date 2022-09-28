be6a5d58caece74ee12648beb579777542f5b77e
jin-co <lilijk@hanmail.net>
2022-09-28 18:47:21 +0800

    Added delete

--- React/test-post/src/components/FeedbackItem.js
+++ React/test-post/src/components/FeedbackItem.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from '../layout/Card'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 
-const FeedbackItem = ({ feed }) => {
+
+const FeedbackItem = ({ feed, handleDelete }) => {
   return (
     <Card>
-      <p className=''>{feed.rating}</p>
+      <p className="">{feed.rating}</p>
+      <FaTimes className="close" color="red" onClick={() => handleDelete(feed.id)} />
+      <FaEdit className="edit" color="purple" />
       <p>{feed.text}</p>
     </Card>
   )
