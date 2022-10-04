7069691ca096117676039b6e52bd19d6ada5df78
jin-co <lilijk@hanmail.net>
2022-10-04 19:52:11 +0800

    Adding Reducer

--- React/test-post/src/context/FeedContext.js
+++ React/test-post/src/context/FeedContext.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { createContext, useState } from 'react'
+import { createContext, useState, useReducer } from 'react'
 import FeedbackData from '../data/FeedbackData'
 import { v4 as uuid } from 'uuid'
+import Reducer from './Reducer'
 
 const FeedContext = createContext()
 
 export const FeedProvider = (props) => {
   const [feed, setFeed] = useState(FeedbackData)
 
+  const initialState = {
+    feeds: [feed],
+    feed: {},
+  }
+
+  const [state, dispatch] = useReducer(Reducer, initialState)
+
   const deleteFeed = (id) => {
     setFeed(feed.filter((f) => f.id !== id))
+    dispatch({
+      type: 'GET_FEEDS',
+      payload: id
+    })
   }
 
   const addFeed = (newFeed) => {
@@ -18,11 +30,15 @@ export const FeedProvider = (props) => {
   }
 
   return (
-    <FeedContext.Provider value={{
-      feed,
-      addFeed,
-      deleteFeed
-    }}>{props.children}</FeedContext.Provider>
+    <FeedContext.Provider
+      value={{
+        feed,
+        addFeed,
+        deleteFeed,
+      }}
+    >
+      {props.children}
+    </FeedContext.Provider>
   )
 }
 
