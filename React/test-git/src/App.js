3e3700771cd8c949cf080b7353b3fb1a0abc2555
jin-co <lilijk@hanmail.net>
2022-10-08 10:26:17 +0800

    Added footer

--- React/test-git/src/App.js
+++ React/test-git/src/App.js
@@ -3,12 +3,13 @@ import About from './pages/About'
 import Notfound from './pages/Notfound'
 import User from './pages/User'
 import Nav from './components/layout/Nav'
+import Footer from './components/layout/Footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col">
+      <div className="flex flex-col justify-between h-screen">
         <Nav />
         <main className="container mx-auth px-1">
           <Routes>
@@ -18,6 +19,7 @@ const App = () => {
             <Route path="/*" element={<Notfound />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   )
---
+++ React/test-git/src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from '../assets/spinner.gif'
+
+const Spinner = () => {
+  return (
+    <div className="w-100 mt-20">
+      <img src={spinner} alt="" width={180} className="text-center mx-auto" />
+    </div>
+  )
+}
+
+export default Spinner
