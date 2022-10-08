b9fc5031116608ec3286378d15106f288ee8f363
jin-co <lilijk@hanmail.net>
2022-10-08 10:19:46 +0800

    Added nav

--- React/test-git/src/App.js
+++ React/test-git/src/App.js
@@ -1,5 +1,26 @@
+import Home from './pages/Home'
+import About from './pages/About'
+import Notfound from './pages/Notfound'
+import User from './pages/User'
+import Nav from './components/layout/Nav'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 const App = () => {
-  return <div>gg</div>
+  return (
+    <Router>
+      <div className="flex flex-col">
+        <Nav />
+        <main className="container mx-auth px-1">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/user/:id" element={<User />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
 }
 
 export default App
