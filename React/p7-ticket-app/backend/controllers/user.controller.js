d594a4abf03dcf17279195512a8cde961054ef49
jin-co <lilijk@hanmail.net>
2022-10-11 20:08:07 +0800

    Working on server

---
+++ React/p7-ticket-app/backend/controllers/user.controller.js
@@ -0,0 +1,12 @@
+const registerUser = (req, res) => {
+  res.send('hhhhhhh')
+}
+
+const loginUser = (req, res) => {
+  res.send('hhhhhhh')
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+}
--- React/p7-ticket-app/backend/server.js
+++ React/p7-ticket-app/backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const doteve = require('dotenv').config()
+const dotenv = require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT | 5000
+
+app.use(express.json())
 
 app.get('/', (req, res, next) => {
   res.send('hh')
 })
+
+app.use('/api/users', require('./routes/user.route'))
 
 app.listen(PORT, () => console.log('server started' + PORT))
