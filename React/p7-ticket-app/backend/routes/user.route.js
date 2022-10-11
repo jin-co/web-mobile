ab968301377735aecbce598dd9e5db4b1e4db989
jin-co <lilijk@hanmail.net>
2022-10-11 20:17:32 +0800

    Update

--- React/p7-ticket-app/backend/routes/user.route.js
+++ React/p7-ticket-app/backend/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {registerUser, loginUser} = require('../controllers/user.controller')
 
-router.post('', registerUser)
+router.post('/register', registerUser)
 
 router.post('/login', loginUser)
 
