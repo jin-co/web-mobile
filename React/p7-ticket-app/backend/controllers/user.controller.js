763540f2410bb4834f55fd20fd41ce50d1fc3bfa
jin-co <lilijk@hanmail.net>
2022-10-11 20:17:38 +0800

    Update

--- React/p7-ticket-app/backend/controllers/user.controller.js
+++ React/p7-ticket-app/backend/controllers/user.controller.js
@@ -1,7 +1,9 @@
+// @path /api/users/register
 const registerUser = (req, res) => {
   res.send('hhhhhhh')
 }
 
+// @path /api/users/login
 const loginUser = (req, res) => {
   res.send('hhhhhhh')
 }
