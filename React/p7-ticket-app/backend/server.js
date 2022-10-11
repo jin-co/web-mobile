bf4898e954c2ff37b18380afb7034588dea4cfea
jin-co <lilijk@hanmail.net>
2022-10-11 19:57:18 +0800

    Added server

--- React/p7-ticket-app/backend/server.js
+++ React/p7-ticket-app/backend/server.js
@@ -1,1 +1,10 @@
-console.log('Server')
+const express = require('express')
+const doteve = require('dotenv').config()
+const app = express()
+const PORT = process.env.PORT | 5000
+
+app.get('/', (req, res, next) => {
+  res.send('hh')
+})
+
+app.listen(PORT, () => console.log('server started' + PORT))
