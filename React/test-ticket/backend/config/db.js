7d186d83971903c5af26521830c0148027e9ca3b
jin-co <lilijk@hanmail.net>
2022-10-12 20:11:19 +0800

    Update

--- React/test-ticket/backend/config/db.js
+++ React/test-ticket/backend/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const connectDB = () => {
   try {
     const conn = mongoose.connect(process.env.MONG)
+    console.log('connected')
   } catch {
     console.log('failed')
   }
