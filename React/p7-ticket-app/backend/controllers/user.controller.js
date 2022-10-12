a509541fbdc51bcda5149bc9318bd4ca6ff4816d
jin-co <lilijk@hanmail.net>
2022-10-12 23:02:05 +0800

    Added register logic

--- React/p7-ticket-app/backend/controllers/user.controller.js
+++ React/p7-ticket-app/backend/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user.model')
 
 // @path /api/users/register
 const registerUser = asyncHandler(async (req, res) => {
@@ -6,6 +8,32 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('what!')
+  }
+
+  const userExists = await User.findOne({ email: email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User exists')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid')
   }
 })
 
