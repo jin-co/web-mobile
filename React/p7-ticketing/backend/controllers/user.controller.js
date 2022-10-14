c3131f4d9e2d71f241982d389699e89deefc0714
jin-co <lilijk@hanmail.net>
2022-10-14 23:18:04 +0800

    Adding backend structure

---
+++ React/p7-ticketing/backend/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export const signUP = (req, res, next) => {
+
+}
+
+export const login = (req, res, next) => {
+
+}
--- React/p7-ticketing/backend/routers/user.router.js
+++ React/p7-ticketing/backend/routers/user.router.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const bcryptjs = require('bcryptjs')
+const { signUP, login } = require('../controllers/user.controller')
 
-router.use('/signup', (req, res, next) => {
-  
-})
+router.use('/signup', signUP)
 
-router.use('/login', (req, res, next) => {
-
-})
+router.use('/login', login)
 
 module.exports = router
