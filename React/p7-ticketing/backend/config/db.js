50f6fc427d06ff96eb1a0aa006ebd835d4b0d6c6
jin-co <lilijk@hanmail.net>
2022-10-14 23:13:18 +0800

    Update

--- React/p7-ticketing/backend/config/db.js
+++ React/p7-ticketing/backend/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONG_KEY) 
   } catch (error) {
-    
+    process.exit(1)
   }   
 } 
 module.exports = connectDB
