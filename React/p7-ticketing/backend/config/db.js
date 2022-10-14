c747c90121d0a96e97aa101ed3b38d27dd99a6d6
jin-co <lilijk@hanmail.net>
2022-10-14 22:55:17 +0800

    Update

--- React/p7-ticketing/backend/config/db.js
+++ React/p7-ticketing/backend/config/db.js
@@ -1,11 +1,2 @@
 const mongoose = require('mongoose')
-const userSchema = mongoose.Schema({
-  name: {type: String, require: [true, 'Add a name']},
-  email: {type: String, require: [true, 'Add a email'], unique: true},
-  email: {type: String, require: true},
-  isAdmin: {type: Boolean, default: false},
-},
-{
-  timeStamp:true
-}
-)
+mongoose.connect(process.env.MONG_KEY)
