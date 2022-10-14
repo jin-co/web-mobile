40892742dd1523315f1e0daa7e76b8af578735a4
jin-co <lilijk@hanmail.net>
2022-10-14 22:58:20 +0800

    Update

--- React/p7-ticketing/backend/server.js
+++ React/p7-ticketing/backend/server.js
@@ -3,8 +3,8 @@ const app = express()
 const port = process.env.PORT | 3000
 
 app.use(express.json())
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
+app.use('/api/users', require('./routers/user.router'))
 
 app.listen(port)
-
