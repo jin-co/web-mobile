c4ed7897f714cded3b20cb3bf509d41865d123dc
jin-co <lilijk@hanmail.net>
2022-10-14 22:59:59 +0800

    Update

--- React/p7-ticketing/backend/server.js
+++ React/p7-ticketing/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT | 3000
+const dbConnect = require('./config/db')
+dbConnect()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
