371475890a7cfa520bee52a313ec04bcf04eb9b4
jin-co <lilijk@hanmail.net>
2022-10-17 22:53:14 +0800

    Update

--- React/p7-ticketing/frontend/src/pages/Register.js
+++ React/p7-ticketing/frontend/src/pages/Register.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { register } from '../features/auth/authSlice'
+import { register, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -15,9 +16,20 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData
   const dispatch = useDispatch()
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const { user, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.auth
   )
+
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess || user) {
+      navigate('/')
+    }
+    dispatch(reset())
+  }, [])
 
   const onChange = (e) => {
     setFormData((prev) => ({
