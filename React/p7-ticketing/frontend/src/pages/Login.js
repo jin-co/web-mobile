fce1506cc4cdf66b930bc3733e1be6c250c061ff
jin-co <lilijk@hanmail.net>
2022-10-18 19:55:49 +0800

    Update

--- React/p7-ticketing/frontend/src/pages/Login.js
+++ React/p7-ticketing/frontend/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { login } from '../features/auth/authSlice'
+import { login, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -12,11 +14,22 @@ const Login = () => {
   })
 
   const { email, password } = formData
-
   const dispatch = useDispatch()
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const navigate = useNavigate()
+
+  const { user, isLoading, isSuccess, message, isError } = useSelector(
     (state) => state.auth
   )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess || user) {
+      navigate('/')
+    }
+    dispatch(reset())
+  }, [])
 
   const onChange = (e) => {
     setFormData((prev) => ({
@@ -35,6 +48,10 @@ const Login = () => {
     dispatch(login(userData))
   }
 
+  if (isLoading) {
+    return <Spinner />
+  }
+  
   return (
     <>
       <section className="heading">
--- React/p7-ticketing/frontend/src/pages/Register.js
+++ React/p7-ticketing/frontend/src/pages/Register.js
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
 import { register, reset } from '../features/auth/authSlice'
 import { useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -51,6 +52,10 @@ const Register = () => {
 
       dispatch(register(userData))
     }
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
