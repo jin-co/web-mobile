8de90cc87f24337ac61865b750f5cb3657c13592
jin-co <lilijk@hanmail.net>
2022-10-18 20:08:36 +0800

    Update

--- React/p7-ticketing/frontend/src/features/auth/authService.js
+++ React/p7-ticketing/frontend/src/features/auth/authService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const API_URL = '/api/users'
+const API_URL = '/api/users/'
 
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL + 'register', userData)
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data))
