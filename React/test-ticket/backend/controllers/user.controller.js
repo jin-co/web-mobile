fe9ac18289fedc1ef53be9cc7a10d6b68bb9b735
jin-co <lilijk@hanmail.net>
2022-10-19 20:07:31 +0800

    Added login logic

--- React/test-ticket/backend/controllers/user.controller.js
+++ React/test-ticket/backend/controllers/user.controller.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 const registerUser = asyncHandler(async (req, res, next) => {
-  const { name, email, password } = req.body  
+  const { name, email, password } = req.body
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('include all the fields')
   }
 
-  const userExists = await User.findOne({ email })  
-  if (userExists) {    
+  const userExists = await User.findOne({ email })
+  if (userExists) {
     res.status(400)
     throw new Error('User exists')
   }
@@ -32,10 +32,22 @@ const registerUser = asyncHandler(async (req, res, next) => {
   } else {
     res.status(400)
     throw new Error('Invalid user data')
-  }  
+  }
 })
 
 const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials')
+  }
   res.send('Login')
 })
 
