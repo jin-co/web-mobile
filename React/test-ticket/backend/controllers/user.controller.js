ce872c6637a09bab710591576221c8b09ec9d2e8
jin-co <lilijk@hanmail.net>
2022-10-19 19:46:20 +0800

    Update

--- React/test-ticket/backend/controllers/user.controller.js
+++ React/test-ticket/backend/controllers/user.controller.js
@@ -1,12 +1,38 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+
 const registerUser = asyncHandler(async (req, res, next) => {
-  const { name, email, password } = req.body
+  const { name, email, password } = req.body  
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('include all the fields')
   }
-  console.log(req.body)
-  res.send('Register')
+
+  const userExists = await User.findOne({ email })  
+  if (userExists) {    
+    res.status(400)
+    throw new Error('User exists')
+  }
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }  
 })
 
 const loginUser = asyncHandler(async (req, res, next) => {
