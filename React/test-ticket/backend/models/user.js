b1db2f3a8df8afc691ec06d44044fedbcba4b151
jin-co <lilijk@hanmail.net>
2022-10-19 19:34:13 +0800

    Added model

---
+++ React/test-ticket/backend/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: [true, 'Add name'] },
+    email: { type: String, required: [true, 'Add email'], unique: true },
+    password: { type: String, required: [true, 'Add password'] },
+    isAdmin: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
