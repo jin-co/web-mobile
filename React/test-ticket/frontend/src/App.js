54f2aede1154e68c3dfbb69c636bfc61a2b4fdf6
jin-co <lilijk@hanmail.net>
2022-10-20 22:58:46 +0800

    Update

--- React/test-ticket/frontend/src/App.js
+++ React/test-ticket/frontend/src/App.js
@@ -1,9 +1,22 @@
+import Home from './app/pages/Home'
+import Register from './app/pages/Register'
+import Login from './app/pages/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      My app
-    </div>
-  );
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
