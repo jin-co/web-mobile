35b51b44a071f20b504f7eac4d0f9ecbd5d81b79
jin-co <lilijk@hanmail.net>
2022-10-20 23:17:58 +0800

    Update

--- React/test-ticket/frontend/src/app/pages/Register.js
+++ React/test-ticket/frontend/src/app/pages/Register.js
@@ -1,8 +1,91 @@
 import React from 'react'
+import { useState } from 'react'
+import { FaUser } from 'react-icons/fa'
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  })
+
+  const { name, email, password, passwordConfirm } = formData
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Create an account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              value={name}
+              name="name"
+              onChange={onChange}
+              placeholder="Enter name"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              name="email"
+              onChange={onChange}
+              placeholder="Enter email"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              name="password"
+              onChange={onChange}
+              placeholder="Enter password"
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="passwordConfirm"
+              value={passwordConfirm}
+              name="passwordConfirm"
+              onChange={onChange}
+              placeholder="Confirm password"
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
