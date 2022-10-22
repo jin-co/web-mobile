5ef67fffa81350f5b1ee671f4615ae495bfe8137
jin-co <lilijk@hanmail.net>
2022-10-22 09:42:57 +0800

    Added auth service logic

---
+++ React/test-ticket/frontend/src/features/auth/authService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const API = '/api/users'
+
+const register = async (userData) => {
+  const response = await axios.post(API, userData)
+  if(response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))    
+  }
+
+  return response.data
+}
+
+const authService = {
+  register
+}
+
+export default authService
--- React/test-ticket/frontend/src/features/auth/authSlice.js
+++ React/test-ticket/frontend/src/features/auth/authSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
+
 const initialState = {
   user: null,
   isError: false,
@@ -10,16 +12,24 @@ const initialState = {
 export const register = createAsyncThunk(
   'auth/register',
   async (user, thunkAPI) => {
-    console.log(user)
+    try {
+      return await authService.register(user)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
   }
 )
 
-export const login = createAsyncThunk(
-  'auth/login',
-  async (user, thunkAPI) => {
-    console.log(user)
-  }
-)
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  console.log(user)
+})
 
 export const authSlice = createSlice({
   name: 'auth',
