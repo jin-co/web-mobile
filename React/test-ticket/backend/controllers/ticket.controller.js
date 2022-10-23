a4cbee47a86070a42121ea51546a79c9652e2c1f
jin-co <lilijk@hanmail.net>
2022-10-23 17:33:36 +0800

    Added crud for tickets

--- React/test-ticket/backend/controllers/ticket.controller.js
+++ React/test-ticket/backend/controllers/ticket.controller.js
@@ -9,7 +9,28 @@ const getTickets = asyncHandler(async (req, res, next) => {
     throw new Error('User not found')
   }
 
-  const ticket = await Ticket.find({ user: req.user.id })
+  const tickets = await Ticket.find({ user: req.user.id })
+  res.status(200).json(tickets)
+})
+
+const getTicket = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not authorized')
+  }
   res.status(200).json(ticket)
 })
 
@@ -36,7 +57,57 @@ const createTicket = asyncHandler(async (req, res, next) => {
   res.status(201).json(ticket)
 })
 
+const deleteTicket = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  await ticket.remove()
+
+  res.status(200).json({ success: true })
+})
+
+const updateTicket = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not authorized')
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+
+  res.status(200).json(updatedTicket)
+})
+
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 }
--- React/test-ticket/backend/routes/ticket.router.js
+++ React/test-ticket/backend/routes/ticket.router.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const { getTickets, createTicket } = require('../controllers/ticket.controller')
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+  deleteTicket,
+  updateTicket,
+} = require('../controllers/ticket.controller')
 
 const protect = require('../middleware/auth.middleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket)
 
 module.exports = router
