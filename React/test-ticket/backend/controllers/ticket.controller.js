d78b8d409d533e911dd149d1d8299e2a496c89d9
jin-co <lilijk@hanmail.net>
2022-10-23 17:18:55 +0800

    Added ticket logic

--- React/test-ticket/backend/controllers/ticket.controller.js
+++ React/test-ticket/backend/controllers/ticket.controller.js
@@ -3,11 +3,37 @@ const User = require('../models/user')
 const Ticket = require('../models/ticket')
 
 const getTickets = asyncHandler(async (req, res, next) => {
-  res.status(200).json({'me' :'get message'})
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.find({ user: req.user.id })
+  res.status(200).json(ticket)
 })
 
 const createTicket = asyncHandler(async (req, res, next) => {
-  res.status(200).json({'me' :'get message'})
+  const { product, description } = req.body
+  if (!product || !description) {
+    res.status(400)
+    throw new Error('Add a product and description')
+  }
+
+  const user = await User.findById(req.user.id)
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  })
+
+  res.status(201).json(ticket)
 })
 
 module.exports = {
