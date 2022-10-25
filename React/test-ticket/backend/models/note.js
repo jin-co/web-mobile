a4e25d25ad582c7df41988fecdb89b0620ec4fb6
jin-co <lilijk@hanmail.net>
2022-10-25 22:53:37 +0800

    Added note model

---
+++ React/test-ticket/backend/models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const noteSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Ticket',
+    },
+    text: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Add some text'],
+      ref: 'Ticket',
+    },
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+    staffId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Note', noteSchema)
