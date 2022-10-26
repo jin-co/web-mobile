a2c0696a4b39dcb25b77850cb30392af56bebf2b
jin-co <lilijk@hanmail.net>
2022-10-26 21:24:20 +0800

    Update

---
+++ React/test-ticket/frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import noteService from './noteService'
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
+
+export const getNotes = createAsyncThunk(
+  'notes/getAll',
+  async (ticketId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+      return await noteService.getNotes(ticketId)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+export const noteSlice = createSlice({
+  name: 'note',
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNotes.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getNotes.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isLoading = true
+        state.notes = action.payload
+      })
+      .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false
+        state.isLoading = true
+        state.isLoading = true
+      })
+  },
+})
+
+export const { reset } = noteSlice.actions
+export default noteSlice.reducer
