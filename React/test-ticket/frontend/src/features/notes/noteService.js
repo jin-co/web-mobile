5da26ce360a579484422fb7397f5b85cacd9a241
jin-co <lilijk@hanmail.net>
2022-10-26 21:24:39 +0800

    Added note slice

---
+++ React/test-ticket/frontend/src/features/notes/noteService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const API = 'api/tickets/'
+const getNotes = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.get(API + ticketId + '/notes', config)
+  return response.data
+}
+
+const noteService = {
+  getNotes,
+}
+
+export default noteService
