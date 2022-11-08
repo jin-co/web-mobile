2e6faa3885dcd08349d3e11f6d6bf8d119ba920c
jin-co <lilijk@hanmail.net>
2022-11-08 18:58:50 +0800

    Added card, button comps

--- React/test-feed/src/pages/Home.js
+++ React/test-feed/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import FeedData from '../data/FeedbackData'
 import FeedList from '../components/feeds/FeedList'
+import { useState } from 'react'
+import FeedStats from '../components/feeds/FeedStats'
+import FeedForm from '../components/feeds/FeedForm'
 
 const Home = () => {
+  const [feed, setFeed] = useState([FeedData])
   return (
-    <div>Home</div>
+    <div>
+      <FeedStats />
+      <FeedForm />
+      <FeedList />
+    </div>
   )
 }
 
