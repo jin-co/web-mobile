7622733fbfdeba4ef9ccca02c3917400ce9c81da
jin-co <lilijk@hanmail.net>
2022-11-10 19:31:18 +0800

    Added button ui

--- React/test-feed/src/components/feeds/FeedForm.js
+++ React/test-feed/src/components/feeds/FeedForm.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import RatingSelect from './RatingSelect'
+import Card from '../Card'
+import Button from '../Button'
 
 const FeedForm = ({ feed }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>
+    <Card>
       <RatingSelect />
-    </div>
+
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" />
+        <Button type="submit">Add</Button>
+      </form>
+    </Card>
   )
 }
 
