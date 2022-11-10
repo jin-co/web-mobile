2e833ab6fb51dcfedef99e0a372eeb6dfaa4b9bf
jin-co <lilijk@hanmail.net>
2022-11-10 19:53:31 +0800

    Added form validation

--- React/test-feed/src/components/feeds/FeedForm.js
+++ React/test-feed/src/components/feeds/FeedForm.js
@@ -2,18 +2,35 @@ import React from 'react'
 import RatingSelect from './RatingSelect'
 import Card from '../Card'
 import Button from '../Button'
+import { useState } from 'react'
 
 const FeedForm = ({ feed }) => {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+
+  const handleInput = (e) => {
+    setText(e.target.value)    
+    console.log()
+    if (e.target.value.length < 10) {
+      setBtnDisabled(true)
+    } else {
+      setBtnDisabled(false)      
+    }    
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
   }
   return (
     <Card>
-      <RatingSelect />
+      <RatingSelect rating={(rating) => {setRating(rating)}} />
 
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" />
-        <Button type="submit">Add</Button>
+        <input type="text" onChange={handleInput} value={text} />
+        <Button type="submit" btnDisabled={btnDisabled}>
+          Add
+        </Button>
       </form>
     </Card>
   )
