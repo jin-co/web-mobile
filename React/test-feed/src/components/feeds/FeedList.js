9f22a4a2676d77f47161b2cdfb032b565d057a20
jin-co <lilijk@hanmail.net>
2022-11-12 09:25:05 +0800

    Added animation

--- React/test-feed/src/components/feeds/FeedList.js
+++ React/test-feed/src/components/feeds/FeedList.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import FeedItem from './FeedItem'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const FeedList = ({ feed, handleDelete }) => {
   return (
     <>
-      {feed.map((f) => (
-        <FeedItem key={f.id} feed={f} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feed.map((f) => (
+          <motion.div
+            key={f.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedItem key={f.id} feed={f} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </>
   )
 }
