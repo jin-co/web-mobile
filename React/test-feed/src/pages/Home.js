106b5bfda1a3bfedc83c6153ea20df96880de6ce
jin-co <lilijk@hanmail.net>
2022-11-12 09:07:21 +0800

    Added add, delete feed

--- React/test-feed/src/pages/Home.js
+++ React/test-feed/src/pages/Home.js
@@ -7,6 +7,15 @@ import FeedForm from '../components/feeds/FeedForm'
 
 const Home = () => {
   const [feed, setFeed] = useState(FeedData)
+
+  const addFeed = (newFeed) => {
+    setFeed([newFeed, ...feed])
+  }
+
+  const deleteFeed = (id) => {
+    setFeed(feed.filter((f) => f.id !== id))
+  }
+
   return (
     <div>
       <FeedStats feed={feed} />
