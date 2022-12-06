d9fe566a727ffe7b5f53df8b0991e2466724e92c
jin-co <lilijk@hanmail.net>
2022-12-06 19:36:41 +0800

    Added routes

--- React/test-housing/src/App.js
+++ React/test-housing/src/App.js
@@ -1,13 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
-im
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-  <>
-<h1></h1>
-  </>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Explore />} />
+          <Route exact path="/offers" element={<Offers />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/sign-in" element={<SignIn />} />
+          <Route exact path="/sign-up" element={<SignUp />} />
+          <Route exact path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
