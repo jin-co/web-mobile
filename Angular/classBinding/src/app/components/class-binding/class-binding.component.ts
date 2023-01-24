2070a5b2c80024f19138e3501bcda99ead9561d1
jin-co <lilijk@hanmail.net>
2023-01-24 20:15:41 +0800

    Complete

---
+++ Angular/classBinding/src/app/components/class-binding/class-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class-binding',
+  templateUrl: './class-binding.component.html',
+  styleUrls: ['./class-binding.component.css'],
+})
+export class ClassBindingComponent implements OnInit {
+  bgMode: string = 'dark';
+  device: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
