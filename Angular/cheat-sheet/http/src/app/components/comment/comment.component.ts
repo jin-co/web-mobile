d11f133beafc61492368fac01b8b4d151b4de31b
jin-co <lilijk@hanmail.net>
2023-01-26 23:16:37 +0800

    Added server

--- Angular/cheat-sheet/http/src/app/components/comment/comment.component.ts
+++ Angular/cheat-sheet/http/src/app/components/comment/comment.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Comment } from 'src/app/models/comment';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent implements OnInit {
-
-  constructor() { }
+  comments:Comment[] = []
+  
+  constructor(private commentService: CommentService) {}
 
   ngOnInit(): void {
+    this.commentService.getComments()
+    this.commentService.commentsUpdateListener().subscribe(comments => {
+      console.log(comments)
+    })
   }
-
 }
--- Angular/cheat-sheet/http/src/app/services/comment.service.ts
+++ Angular/cheat-sheet/http/src/app/services/comment.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Comment } from '../models/comment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  URL = 'https://jsonplaceholder.typicode.com/comments'    
+  // comments:Comment[] = []
+  comments = new Subject<Comment[]>
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getComments() {
+    this.http.get<Comment[]>(this.URL).subscribe(comments => {
+      console.log('back: ', comments)
+        this.comments.next(comments)
+    })
+  }
+
+  commentsUpdateListener() {
+    return this.comments.asObservable()
+  }
 }
