cc945cb87457f41c8e53cce05982d9aa63707633
jin-co <lilijk@hanmail.net>
2023-02-03 22:42:12 +0800

    Update

---
+++ TypeScript/practice.js
@@ -0,0 +1,6 @@
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+    var result = num1 + num2;
+    return result;
+}
+document.body.innerHTML = addNumbers(1, 2);
---
+++ TypeScript/practice.ts
@@ -0,0 +1,7 @@
+function addNumbers(num1, num2) {
+  console.log(num1 + num2)
+  const result = num1 + num2  
+  return result
+}
+
+document.body.innerHTML = addNumbers(1, 2)
