caca1e9a2c66717cf65e2a48f9ead4fa319a372b
jin-co <lilijk@hanmail.net>
2023-05-08 10:38:06 +0800

    Working on feed

--- React/test-feed-t/src/components/feed/FeedForm.js
+++ React/test-feed-t/src/components/feed/FeedForm.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Card } from '../shared/Card'
+import { FeedRatingSelect } from './FeedRatingSelect'
+import { Button } from '../shared/Button'
 
 export const FeedForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>FeedForm</div>
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <FeedRatingSelect />
+        <div className="input-group">
+          <input
+
+          />
+        </div>
+        <Button type='submit'>Add</Button>
+      </form>
+    </Card>
   )
 }
