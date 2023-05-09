eaad0ce3819107aa81687d3d6865cf982ce22b8a
jin-co <lilijk@hanmail.net>
2023-05-09 09:41:54 +0800

    Added rating selection

--- React/test-feed-t/src/components/feed/FeedForm.js
+++ React/test-feed-t/src/components/feed/FeedForm.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from '../shared/Card'
 import { FeedRatingSelect } from './FeedRatingSelect'
 import { Button } from '../shared/Button'
 
 export const FeedForm = () => {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(0)
+  const [isDisabled, setIsDisabled] = useState(true)
+
   const handleSubmit = (e) => {
     e.preventDefault()
   }
+
+  const handleChange = (e) => {    
+    if (e.target.value.trim().length > 10) {
+      setIsDisabled(false)
+      setText(e.target.value)
+      console.log(rating, text);
+    } else {
+      setIsDisabled(true)
+    }
+  }
+
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <FeedRatingSelect />
+        <FeedRatingSelect inComingRating={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
-
+            onChange={handleChange}
           />
         </div>
-        <Button type='submit'>Add</Button>
+        <Button isDisabled={isDisabled} type='submit'>Add</Button>
       </form>
     </Card>
   )
--- React/test-feed-t/src/components/feed/FeedRatingSelect.js
+++ React/test-feed-t/src/components/feed/FeedRatingSelect.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const FeedRatingSelect = () => {
+export const FeedRatingSelect = ({ inComingRating }) => {
+  const [rating, setRating] = useState(10)
+
+  const handleChange = (e) => {
+    inComingRating(+e.target.value)
+    setRating(+e.target.value)
+  }
+
   return (
     <ul className='rating'>
       {
         Array.from({ length: 10 }, (_, i) => (
           <li>
-          <input type="radio" />
+            <input type="radio" value={i + 1} id={i} checked={rating == i + 1} onChange={handleChange} />
+            <label htmlFor={i}>{i + 1}</label>
           </li>
         ))
       }
