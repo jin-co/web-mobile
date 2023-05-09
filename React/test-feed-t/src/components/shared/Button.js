d836ae8d54db50368407181605dc8d3045472ab5
jin-co <lilijk@hanmail.net>
2023-05-09 09:27:11 +0800

    Added btn, card comp

--- React/test-feed-t/src/components/shared/Button.js
+++ React/test-feed-t/src/components/shared/Button.js
@@ -1,7 +1,13 @@
+import { isDisabled } from '@testing-library/user-event/dist/utils'
 import React from 'react'
 
-export const Button = () => {
+export const Button = ({version, isDisabled}) => {
   return (
-    <div>Button</div>
-  )
+    <button className={`btn btn-${version}`} disabled={isDisabled}>Button</button>
+  )  
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  isDisabled: true
 }
