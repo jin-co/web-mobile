e948b4dd921ca4e1df2c68b11a817e069be1e9e5
jin-co <lilijk@hanmail.net>
2023-05-10 08:54:37 +0800

    Added feed list

--- React/test-feed-t/src/components/feed/FeedItem.js
+++ React/test-feed-t/src/components/feed/FeedItem.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Card } from '../shared/Card'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 
-export const FeedItem = () => {
+export const FeedItem = ({ feed }) => {
+  const deleteFeed = () => {
+
+  }
   return (
-    <div>FeedItem</div>
+    <Card>    
+      <button className='close' onClick={() => deleteFeed(feed.id)}>
+        <FaTimes />
+      </button>
+      <button className='edit'>
+        <FaEdit />
+      </button>
+      <div className="num-display">{feed.rating}</div>
+      <div className="text-display">{feed.text}</div>
+    </Card>
   )
 }
