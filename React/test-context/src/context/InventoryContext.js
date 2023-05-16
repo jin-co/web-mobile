187bd76d4352bba120084cfaab70b1bbc89a5f7b
jin-co <lilijk@hanmail.net>
2023-05-16 09:23:53 +0800

    Update

---
+++ React/test-context/src/context/InventoryContext.js
@@ -0,0 +1,15 @@
+import { createContext } from 'react'
+
+const InventoryContext = createContext()
+
+export const InventoryProvider = ({ children }) => {
+  return (
+    <InventoryContext.Provider value={{
+
+    }}>
+      {children}
+    </InventoryContext.Provider>
+  )
+}
+
+export default InventoryContext
