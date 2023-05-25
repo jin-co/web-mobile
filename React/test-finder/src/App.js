989f7f4641be186860a4d9b38f686cf303248372
jin-co <lilijk@hanmail.net>
2023-05-25 12:18:52 +0800

    Added tailwind

--- React/test-finder/src/App.js
+++ React/test-finder/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Nav } from './components/layout/Nav'
+import { Footer } from './components/layout/Footer'
+import { Alert } from './components/layout/Alert'
+import { Home } from './pages/Home'
+import { Notfound } from './pages/Notfound'
+import { About } from './pages/About'
+import { User } from './pages/User'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen bg-midnight">
+        <Nav />
+        <main className="container mx-auto px-3 pb-12">
+          <Alert />
+          <Routes>          
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route path="/user/:login" element={<User />} />
+            <Route exact path="/notfound" element={<Notfound />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
 }
-
-export default App;
