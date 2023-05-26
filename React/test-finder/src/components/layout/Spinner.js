329127b808df13ab5f7f027b7adcd20dcdc033c7
jin-co <lilijk@hanmail.net>
2023-05-26 11:55:37 +0800

    Update

--- React/test-finder/src/components/layout/Spinner.js
+++ React/test-finder/src/components/layout/Spinner.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import spin from '../assets/spinner.gif'
 
 export const Spinner = () => {
   return (
-    <div>Spinner</div>
+    <div className='w-100 mt-20' alt='loading...'>
+      <img src={spin} alt="" width={180} className='text-center mx-auto' />
+    </div>
   )
 }
