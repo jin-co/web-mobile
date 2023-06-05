8fea4161d4a06b8fefecfd2920ee46c3e7dc3bc7
jin-co <lilijk@hanmail.net>
2023-06-05 14:52:32 +0800

    Completed

--- React/test-git/src/context/user/UserContext.js
+++ React/test-git/src/context/user/UserContext.js
@@ -2,6 +2,7 @@ import React, { createContext, useEffect, useState, useReducer } from 'react'
 import { UserReducer } from './UserReducer'
 
 const UserContext = createContext()
+const POST_URL = process.env.REACT_APP_POST
 
 export const UserProvider = ({ children }) => {
   const initialState = {
@@ -11,7 +12,34 @@ export const UserProvider = ({ children }) => {
     isLoading: false
   }
 
-  const [state, dispatch] = useReducer(UserReducer, initialState)  
+  const [posts, setPosts] = useState([])
+
+  const [state, dispatch] = useReducer(UserReducer, initialState)
+
+  useEffect(() => {
+    addPost()
+    fetchPosts()
+  }, [])
+
+  const fetchPosts = async () => {
+    const res = await fetch(POST_URL)
+    const data = await res.json()
+    console.log(data)
+  }
+
+  const addPost = async () => {
+    const newPost = {
+      id: 2,
+      title: 'Some!',
+      author: "What"
+    }
+    await fetch(POST_URL + 2, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }      
+    })
+  }
 
   return (
     <UserContext.Provider value={{
