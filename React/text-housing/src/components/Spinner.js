4ade4044d4e68984b1453bac1221ba7509e87eea
jin-co <lilijk@hanmail.net>
2023-06-15 11:32:00 +0800

    Clean

--- React/text-housing/src/components/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Spinner = () => {
-  return (
-    <div className="loadingSpinnerContainer">
-      <div className="loadingSpinner"></div>
-    </div>
-  )
-}
