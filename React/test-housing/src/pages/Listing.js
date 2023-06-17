5b3da7a65cb330da49b99257febbd17500dccee7
jin-co <lilijk@hanmail.net>
2023-06-17 21:53:38 +0800

    Update

--- React/test-housing/src/pages/Listing.js
+++ React/test-housing/src/pages/Listing.js
@@ -28,100 +28,100 @@ export const Listing = () => {
 
   return (
     <></>
-    // <main>
-    //   <Swiper slidesPerView={1} pagination={{ clickable: true }}>
-    //     {listing.imageUrls.map((urls, index) => (
-    //       <SwiperSlide key={index}>
-    //         <div
-    //           style={{
-    //             background: `url(${listing.imageUrls[index]}) center no-repeat`,
-    //             backgroundSize: 'cover',
-    //             height: '300px',
-    //           }}
-    //           className="swiperSlideDiv"
-    //         ></div>
-    //       </SwiperSlide>
-    //     ))}
-    //   </Swiper>
+    <main>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((urls, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+                height: '300px',
+              }}
+              className="swiperSlideDiv"
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-    //   <div
-    //     className="shareIconDiv"
-    //     onClick={() => {
-    //       navigator.clipboard.writeText(window.location.href)
-    //       setShareLinkCopied(true)
-    //       setTimeout(() => {
-    //         setShareLinkCopied(false)
-    //       }, 2000)
-    //     }}
-    //   >
-    //     <img src={shareIcon} alt="" />
-    //   </div>
-    //   {shareLinkCopied && <p className="linkCopied">Link Copied</p>}
-    //   <div className="listingDetails">
-    //     <p className="listingName">
-    //       {listing.name} - $
-    //       {listing.offer
-    //         ? listing.discountedPrice
-    //           .toString()
-    //           .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-    //         : listing.regularPrice
-    //           .toString()
-    //           .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-    //     </p>
-    //     <p className="listingLocation">{listing.location}</p>
-    //     <p className="listingType">
-    //       For {listing.type === 'rent' ? 'Rent' : 'Sale'}
-    //     </p>
-    //     {listing.offer && (
-    //       <p className="discountPrice">
-    //         ${listing.regularPrice - listing.discountedPrice} discount
-    //       </p>
-    //     )}
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
+        <img src={shareIcon} alt="" />
+      </div>
+      {shareLinkCopied && <p className="linkCopied">Link Copied</p>}
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
 
-    //     <ul className="listingDetailsList">
-    //       <li>
-    //         {listing.bedrooms > 1 ? `${listing.bedrooms}bedrooms` : '1 bedroom'}
-    //       </li>
-    //       <li>
-    //         {listing.bathrooms > 1
-    //           ? `${listing.bathrooms}bathrooms`
-    //           : '1 bathroom'}
-    //       </li>
-    //       <li>{listing.parking && 'Parking spot'}</li>
-    //       <li>{listing.furnished && 'Furnished'}</li>
-    //     </ul>
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1 ? `${listing.bedrooms}bedrooms` : '1 bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms}bathrooms`
+              : '1 bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
 
-    //     <p className="listingLocationTitle">Location</p>
+        <p className="listingLocationTitle">Location</p>
 
-    //     <div className="leafletContainer">
-    //       <MapContainer
-    //         style={{ height: '100%', width: '100%' }}
-    //         center={[listing.geolocation.lat, listing.geolocation.lng]}
-    //         zoom={13}
-    //         scrollWheelZoom={false}
-    //       >
-    //         <TileLayer
-    //           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    //           url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
-    //         />
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
 
-    //         <Marker
-    //           position={[listing.geolocation.lat, listing.geolocation.lng]}
-    //         >
-    //           <Popup>{listing.location}</Popup>
-    //         </Marker>
-    //       </MapContainer>
-    //     </div>
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
-    //     {auth.currentUser?.uid !== listing.userRef && (
-    //       <Link
-    //         to={`/contact/${listing.userRef}?listingName=${listing.name}`}
-    //         className="primaryButton"
-    //       >
-    //         Contact landlord
-    //       </Link>
-    //     )}
-    //   </div>
-    // </main>
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className="primaryButton"
+          >
+            Contact landlord
+          </Link>
+        )}
+      </div>
+    </main>
   )
 }
