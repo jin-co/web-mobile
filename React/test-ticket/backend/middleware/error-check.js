78916ee156e6ecf031e832592a180e224ffb38e5
jin-co <lilijk@hanmail.net>
2023-06-21 20:26:12 +0800

    Clean

--- React/test-ticket/backend/middleware/error-check.js
+++
@@ -1,9 +0,0 @@
-const errorCheck = (err, req, res, next) => {
-  const status = res.statusCode ? res.statusCode : 500
-  res.status(status).json({
-    message: err.message,
-    stack: err.stack
-  })
-}
-
-module.exports = errorCheck
--- React/test-ticket/backend/routes/note.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router({mergeParams: true})
-
-const {getNotes, addNote} = require('../controller/note')
-router.get('/', getNotes)
-router.post('/', addNote)
-module.exports = router
