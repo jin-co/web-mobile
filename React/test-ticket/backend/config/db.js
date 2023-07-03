115d912a52136fbcbc0133d949086e72e0175823
jin-co <lilijk@hanmail.net>
2023-07-03 09:58:17 +0800

    Clean

--- React/test-ticket/backend/config/db.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONG).then(() => console.log('connected')).catch(() => console.log('failed'))
--- React/test-ticket/backend/router/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { join, login } = require('../controller/user')
-
-router.post('/join', join)
-router.post('/login', login)
-
-module.exports = router
