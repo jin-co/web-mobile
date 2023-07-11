c8f7c876023da9215fe985f7b622a18b6c89baae
jin-co <lilijk@hanmail.net>
2023-07-11 14:41:40 +0800

    Added server

--- React/test-feed/src/context/Feed.js
+++ React/test-feed/src/context/Feed.js
@@ -1,30 +1,57 @@
-import { useState, createContext } from 'react'
+import { useState, createContext, useEffect } from 'react'
 import { Feed } from '../data/FeedData'
 import { v4 as uuid } from 'uuid'
+
+const url = process.env.REACT_APP_URL + 'feeds/'
 
 const FeedContext = createContext()
 
 export const FeedProvider = (props) => {
-  const [feed, setFeed] = useState(Feed)
+  const [feed, setFeed] = useState([])
   const [getFeed, setGetFeed] = useState({
     feed: {},
     isEdit: false
   })
 
-  const addFeed = (newFeed) => {
+  useEffect(() => {
+    getFeeds()
+  }, [])
+
+  const getFeeds = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setFeed(data)
+  }
+
+  const addFeed = async (newFeed) => {
     newFeed.id = uuid()
     setFeed([newFeed, ...feed])
+    await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newFeed)
+    })
   }
 
-  const deleteFeed = (id) => {
+  const deleteFeed = async (id) => {
     setFeed(feed.filter(f => f.id !== id))
+    await fetch(url + id, {
+      method: 'DELETE',
+    })
   }
 
-  const updateFeed = (id, newFeed) => {
+  const updateFeed = async (id, newFeed) => {
     setFeed(feed.map(f => f.id === id ? { ...f, ...newFeed } : f))
     setGetFeed({
       feed: {},
       isEdit: false
+    })
+    await fetch(url + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeed)
     })
   }
 
