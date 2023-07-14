f0c7744dbe650ec1b3c98b201beb8073cf09bcb3
jin-co <lilijk@hanmail.net>
2023-07-14 12:18:24 +0800

    Completed

--- React/test-git/src/context/git/GitContext.js
+++ React/test-git/src/context/git/GitContext.js
@@ -5,16 +5,44 @@ const GitContext = createContext()
 
 export const GitProvider = ({ children }) => {
   const [users, setUsers] = useState([])
+  const [user, setUser] = useState({})
+  const [repos, setRepos] = useState([])
+
   const getUsers = async (text) => {
     const res = await fetch(url + 'search/users?q=' + text)
     const { items } = await res.json()
     setUsers(items)
   }
 
+  const getUser = async (login) => {
+    const res = await fetch(url + 'users/' + login)
+    if (res.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await res.json()
+      console.log(data)
+      setUser(data)
+    }
+  }
+
+  const getRepos = async (login) => {
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10
+    })
+    const res = await fetch(url + `users/${login}/repos?${params}`)
+    const data = await res.json()
+    setRepos(data)
+  }
+
   return (
     <GitContext.Provider value={{
       users,
-      getUsers
+      repos,
+      user,
+      getUsers,
+      getUser,
+      getRepos,
     }}>
       {children}
     </GitContext.Provider>
