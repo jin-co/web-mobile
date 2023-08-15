123d57835a651b125de486c78d9977a973bf446e
jin-co <lilijk@hanmail.net>
2023-08-16 07:01:32 +0900

    Working on auth

--- React/test-housing/src/pages/Profile.js
+++ React/test-housing/src/pages/Profile.js
@@ -1,7 +1,116 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { Link } from 'react-router-dom'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+import { ListingItem } from '../components/ListingItem'
 
 export const Profile = () => {
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
+  })
+  useEffect(() => {
+    const auth = getAuth()
+    console.log(auth)
+  }, [])
+
+  const { name, email } = formData
+
+  const onLogout = () => {
+
+  }
+
+  const onSubmit = (e) => {
+
+  }
+
+  const onChange = (e) => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const onDelete = () => {
+
+  }
+
+  const onEdit = () => {
+
+  }
+
   return (
-    <div>Profile</div>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button className="logOut" type="button" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prev) => !prev)
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={handleChange}
+            />
+
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="" />
+          <p>Sell or rent</p>
+          <img src={arrowRight} alt="" />
+        </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className="listingText">Your listings</p>
+            <ul className="listingsList">
+              {listings.map((lis) => (
+                <ListingItem
+                  key={lis.id}
+                  listing={lis.data}
+                  id={lis.id}
+                  onDelete={() => onDelete(lis.id)}
+                  onEdit={() => onEdit(lis.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </main>
+    </div>
   )
 }
--- React/test-housing/src/pages/SignIn.js
+++ React/test-housing/src/pages/SignIn.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { Link } from 'react-router-dom'
+import { OAuth } from '../components/OAuth'
 
 export const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  const { email, password } = formData
+  const handleSubmit = (e) => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
   return (
-    <div>SignIn</div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome</p>
+          <main>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                className="emailInput"
+                placeholder="email"
+                id="email"
+                value={email}
+                onChange={handleChange}
+              />
+
+              <div className="passwordInputDiv">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  className="passwordInput"
+                  placeholder="Password"
+                  id="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+
+                <img
+                  src={visibilityIcon}
+                  alt=""
+                  className="showPassword"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                />
+              </div>
+
+              <div className="container">
+                <Link to="/sign-up" className="forgotPasswordLink">
+                  Sign Up Instead
+                </Link>
+
+                <Link to="/forgot-password" className="forgotPasswordLink">
+                  Forgot Password
+                </Link>
+              </div>
+
+              <div className="signInBar">
+                <p className="signInText">Sign In</p>
+
+                <button className="signInButton">
+                  <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+                </button>
+              </div>
+            </form>
+
+            <OAuth />
+          </main>
+        </header>
+      </div>
+    </>
   )
 }
