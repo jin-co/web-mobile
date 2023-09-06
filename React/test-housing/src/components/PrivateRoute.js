275bab19d928dfdd90dff9a0e074ea75f72d7d89
jin-co <lilijk@hanmail.net>
2023-09-07 08:12:19 +0900

    Completed

--- React/test-housing/src/components/PrivateRoute.js
+++ React/test-housing/src/components/PrivateRoute.js
@@ -2,20 +2,10 @@ import { useEffect, useState } from "react"
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { Spinner } from './Spinner'
 import { Navigate, Outlet } from "react-router-dom"
+import { useAuthStatus } from "../hooks/useAuthStatus"
 
 export const PrivateRoute = () => {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true)
-      }
-      setLoading(false)
-    })
-  }, [])
+  const { loggedIn, loading } = useAuthStatus()
 
   if (loading) return <Spinner />
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />
--- React/test-housing/src/hooks/useAuthStatus.js
+++ React/test-housing/src/hooks/useAuthStatus.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
 
 export const useAuthStatus = () => {
-  return (
-    <div>useAuthStatus</div>
-  )
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true)
+      }
+      setLoading(false)
+    })
+  }, [])
+  return { loggedIn, loading }
 }
