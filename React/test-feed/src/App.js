d2ab96fa7ef24800feeec9b33dbb30b08f0a4c61
jin-co <lilijk@hanmail.net>
2023-10-02 12:10:17 +0900

    Added router

--- React/test-feed/src/App.js
+++ React/test-feed/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Nav } from './components/Nav'
+import {About} from './pages/About'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
