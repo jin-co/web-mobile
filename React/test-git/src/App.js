b4f4b82c0a18844b4b7e78d0d9345eb04c19a0b4
jin-co <lilijk@hanmail.net>
2023-11-27 07:42:43 +0900

    Update

--- React/test-git/src/App.js
+++ React/test-git/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Notfound } from './pages/Notfound'
+import { User } from './pages/User'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home />
+      <About />
+      <Notfound />
+      <User />
+    </div>
   )
 }
