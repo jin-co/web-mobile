4fd128cbf8418973e27270f19dffeb7090d32262
jin-co <lilijk@hanmail.net>
2023-11-28 07:42:44 +0900

    Update

--- React/test-git/src/App.js
+++ React/test-git/src/App.js
@@ -3,14 +3,21 @@ import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Notfound } from './pages/Notfound'
 import { User } from './pages/User'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/layout/Navbar'
 
 export const App = () => {
   return (
     <div>
-      <Home />
-      <About />
-      <Notfound />
-      <User />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/user' element={<User />} />
+          <Route exact path='/*' element={<Notfound />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
