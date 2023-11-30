1237dd7fa31e4087d0ca74192161e8bfbb9ec7c5
jin-co <lilijk@hanmail.net>
2023-12-01 07:42:23 +0900

    Update

--- React/test-git/src/components/layout/Navbar.js
+++ React/test-git/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-2 text-3xl" />
+          <FaGithub className="inline pr-2 text-3xl" />          
           <Link to="/" className="text-lg font-bold">
             Home
           </Link>          
