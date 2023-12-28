c1c33459cde0ec3bb1fa4fa8662089b7d8caa50b
jin-co <lilijk@hanmail.net>
2023-12-28 10:44:07 +0900

    Completed

--- React/test-git/src/components/repos/Repo.js
+++ React/test-git/src/components/repos/Repo.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { RepoItem } from './RepoItem'
 
-export const Repo = () => {
+export const Repo = ({ repos }) => {
   return (
-    <div>Repo</div>
+    <div className="rounded-lg shadow-lg card bg-base-100">
+      <div className="card-body">
+        <h2 className="text-3xl my-4 font-bold card-title">Repositories</h2>
+        {
+          repos.map((repo) => (
+            <RepoItem key={repo.id} repo={repo} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
