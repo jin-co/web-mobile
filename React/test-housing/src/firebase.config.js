6d9ecb887ef301f70b5b60d145b9e85d82665898
jin-co <lilijk@hanmail.net>
2024-01-18 09:36:25 +0900

    Added firebase

---
+++ React/test-housing/src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBpIzA-rbNIHilywco-3GQvvdIEAkjQRk",
+  authDomain: "fire-test-74d76.firebaseapp.com",
+  projectId: "fire-test-74d76",
+  storageBucket: "fire-test-74d76.appspot.com",
+  messagingSenderId: "183642125272",
+  appId: "1:183642125272:web:1a4320c8308bd2ec4578ad"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore()
